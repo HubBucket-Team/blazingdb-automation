b7bd39dfb07ef7974ca9d650d240ce56df67de58
Percy Camilo Triveño Aucahuasi <percy@blazingdb.com>
2018-10-24 04:32:17 -0500

    Finish docker image for the mvp demo

---
+++ docker/blazingsql/libgdf_cffi/build.sh
@@ -0,0 +1,2 @@
+# install
+python setup.py install --single-version-externally-managed --record=record.txt
---
+++ docker/blazingsql/setup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+working_directory=$PWD
+
+blazingsql_files=/tmp/blazing/blazingsql-files
+
+cd /tmp/blazing/ && tar xvf blazingsql-files.tar.gz
+cd $blazingsql_files
+
+# install libgdf and libgdf_cffi
+cp libgdf_cffi/lib/libgdf.so /conda/envs/gdf/lib/
+cd libgdf_cffi/
+cp /tmp/blazing/libgdf_cffi/meta.yaml .
+cp /tmp/blazing/libgdf_cffi/build.sh .
+echo "replacing libgdf_cffi"
+source activate gdf && conda-build .
+pip install .
+echo "libgdf_cffi is ready!"
+
+# install blazingsql
+cd $blazingsql_files
+cp testing-libgdf /usr/bin
+cp blazingdb_orchestator_service /usr/bin
+cp BlazingCalcite.jar /usr/bin
+
+# install pyblazing
+cd pyBlazing
+pip install .
+
+cd $working_directory

a79325f8aaae18036922786e69713193c33c0f01
Percy Camilo Triveño Aucahuasi <percy@blazingdb.com>
2018-11-26 06:05:32 -0500

    Best way to install pysql and setup the docker env

--- docker/blazingsql/setup.sh
+++ docker/blazingsql/setup.sh
@@ -8,57 +8,34 @@ cd $blazingsql_files
 
 source activate cudf
 
-# install libgdf, libgdf/python and cudf
+# Install libgdf
 cudf_dir=$blazingsql_files/cudf
 cp $cudf_dir/libgdf/install/lib/libgdf.so /conda/envs/cudf/lib/
 cp $cudf_dir/libgdf/install/lib/librmm.so /conda/envs/cudf/lib/
+cp $cudf_dir/libgdf/install/lib/libgdf.so /usr/lib/
+cp $cudf_dir/libgdf/install/lib/librmm.so /usr/lib/
+cp /conda/envs/cudf/lib/libNVStrings.so /usr/lib/
+
+# Install libgdf_cffi
 cp -r $cudf_dir/libgdf/install/* $cudf_dir/libgdf/python
 pip install $cudf_dir/libgdf/python
 
+# Install cudf
+pip install $cudf_dir
 
-exit
-cp /tmp/blazing/libgdf_cffi/meta.yaml .
-cp /tmp/blazing/libgdf_cffi/build.sh .
+# Install blazingsql
+blazingdb_ral_artifact_name=testing-libgdf
+blazingdb_orchestrator_artifact_name=blazingdb_orchestator_service
+blazingdb_calcite_artifact_name=BlazingCalcite.jar
 
-echo "### Iniciando conda-build ###"
-#conda-build .
-pip install .
-echo "Custom libgdf_cffi is ready!"
+cp $blazingsql_files/$blazingdb_ral_artifact_name /usr/bin
+cp $blazingsql_files/$blazingdb_orchestrator_artifact_name /usr/bin
+cp $blazingsql_files/$blazingdb_calcite_artifact_name /usr/bin
 
-echo "Installing custom cudf"
-rm -rf /conda/envs/cudf/lib/python3.5/site-packages/pygdf-*
-cd /tmp/blazing/blazingsql-files/cudf/conda-recipes/cudf/ 
-cp /tmp/blazing/cudf/meta.yaml .
-cp /tmp/blazing/cudf/build.sh .
-echo "### Iniciando conda-build cudf ###"
-#conda-build .
-cd $blazingsql_files/cudf/ && pip install .
-python -c "import cudf"
-echo "Custom cudf is ready"
+# Install blazingdb-protocol/python
+pip install $blazingsql_files/blazingdb-protocol/python/
 
-# install blazingsql
-cd $blazingsql_files
-cp testing-libgdf /usr/bin
-cp blazingdb_orchestator_service /usr/bin
-cp BlazingCalcite.jar /usr/bin
-
-# install pyblazing
-echo "### Iniciando blazingdg-protocol/python ###"
-cd blazingdb-protocol/python/
-pip install .
-
-echo "### Iniciando pyblazing ###"
-cd $blazingsql_files
-cd pyBlazing
-pip install .
-
-echo "### Copiando los .so ###"
-mkdir -p /usr/local/nvidia/lib
-cp /conda/envs/cudf/lib/libgdf.so /usr/local/nvidia/lib/
-cp /conda/envs/cudf/lib/librmm.so /usr/local/nvidia/lib/
-cp /conda/envs/cudf/lib/libNVStrings.so /usr/local/nvidia/lib/
-
-#echo "### copiandoo bkp ###"
-#cp -rf $blazingsql_files /root/blazingsql_files/
+# Install pyblazing
+pip install $blazingsql_files/pyBlazing
 
 cd $working_directory

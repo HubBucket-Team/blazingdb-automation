61e4d90b195e45a3c6b3ca33988f36cbfbbc2c15
Percy Camilo Triveño Aucahuasi <percy@blazingdb.com>
2018-11-26 02:30:25 -0500

    Upgrade collect/zip code

--- docker/blazingsql-build/build.sh
+++ docker/blazingsql-build/build.sh
@@ -195,7 +195,8 @@ if [ ! -d build ]; then
     NVSTRINGS_ROOT=$nvstrings_install_dir cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX:PATH=$libgdf_install_dir ..
 fi
 
-cd $cudf_current_dir/cudf/$libgdf_dir/build/
+libgdf_build_dir=$cudf_current_dir/cudf/$libgdf_dir/build/
+cd $libgdf_build_dir
 NVSTRINGS_ROOT=$nvstrings_install_dir cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX:PATH=$libgdf_install_dir ..
 make -j$cudf_parallel install
 
@@ -236,12 +237,14 @@ if [ ! -d build ]; then
     cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX:PATH=$blazingdb_protocol_install_dir ..
 fi
 
-cd $blazingdb_protocol_current_dir/blazingdb-protocol/cpp/build/
+blazingdb_protocol_cpp_build_dir=$blazingdb_protocol_current_dir/blazingdb-protocol/cpp/build/
+cd $blazingdb_protocol_cpp_build_dir
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX:PATH=$blazingdb_protocol_install_dir ..
 make -j$blazingdb_protocol_parallel install
 
 cd $blazingdb_protocol_current_dir/blazingdb-protocol/java
 mvn clean install -Dmaven.test.skip=true
+blazingdb_protocol_java_build_dir=$blazingdb_protocol_current_dir/blazingdb-protocol/java/target/
 
 #END blazingdb-protocol
 
@@ -279,7 +282,8 @@ if [ ! -d build ]; then
     cmake -DCMAKE_BUILD_TYPE=Release -DNVSTRINGS_HOME=$nvstrings_install_dir -DLIBGDF_HOME=$libgdf_install_dir -DBLAZINGDB_PROTOCOL_HOME=$blazingdb_protocol_install_dir ..
 fi
 
-cd $blazingdb_ral_current_dir/blazingdb-ral/build/
+blazingdb_ral_build_dir=$blazingdb_ral_current_dir/blazingdb-ral/build/
+cd $blazingdb_ral_build_dir
 cmake -DCMAKE_BUILD_TYPE=Release -DNVSTRINGS_HOME=$nvstrings_install_dir -DLIBGDF_HOME=$libgdf_install_dir -DBLAZINGDB_PROTOCOL_HOME=$blazingdb_protocol_install_dir ..
 make -j$blazingdb_ral_parallel
 
@@ -318,7 +322,8 @@ if [ ! -d build ]; then
     cmake -DCMAKE_BUILD_TYPE=Release -DBLAZINGDB_PROTOCOL_HOME=$blazingdb_protocol_install_dir ..
 fi
 
-cd $blazingdb_orchestrator_current_dir/blazingdb-orchestrator/build/
+blazingdb_orchestrator_build_dir=$blazingdb_orchestrator_current_dir/blazingdb-orchestrator/build/
+cd $blazingdb_orchestrator_build_dir
 cmake -DCMAKE_BUILD_TYPE=Release -DBLAZINGDB_PROTOCOL_HOME=$blazingdb_protocol_install_dir ..
 make -j$blazingdb_orchestrator_parallel
 
@@ -352,6 +357,7 @@ git pull
 blazingdb_calcite_install_dir=$blazingdb_calcite_current_dir/install
 
 mvn clean install -Dmaven.test.skip=true
+blazingdb_calcite_build_dir=$blazingdb_calcite_current_dir/blazingdb-calcite-application/target/
 
 #END blazingdb-calcite
 
@@ -384,9 +390,43 @@ pyblazing_install_dir=$pyblazing_current_dir/install
 
 #END pyblazing
 
-echo "alla"
+#BEGIN collect/package artifacts
 
+cd $workspace_dir
 
+output=/home/builder/output/blazingsql-files
+
+# Package cudf
+mkdir -p ${output}/cudf/
+cp -r $cudf_current_dir/cudf/* ${output}/cudf/
+rm -rf ${output}/cudf/.git/
+
+# Package blazingdb-ral
+blazingdb_ral_artifact_name=testing-libgdf
+cp $blazingdb_ral_build_dir/$blazingdb_ral_artifact_name $output
+
+# Package blazingdb-orchestrator
+blazingdb_orchestrator_artifact_name=blazingdb_orchestator_service
+cp $blazingdb_orchestrator_build_dir/$blazingdb_orchestrator_artifact_name $output
+
+# Package blazingdb-calcite
+blazingdb_calcite_artifact_name=BlazingCalcite.jar
+cp $blazingdb_calcite_build_dir/$blazingdb_calcite_artifact_name ${output}
+
+# Package blazingdb-protocol/python
+mkdir -p $output/blazingdb-protocol/python/
+cp -r $blazingdb_protocol_current_dir/blazingdb-protocol/python/* $output/blazingdb-protocol/python/
+
+# Package PyBlazing
+mkdir -p ${output}/pyBlazing/
+cp -r $pyblazing_current_dir/pyBlazing/* ${output}/pyBlazing/
+rm -rf ${output}/pyBlazing/.git/
+
+# Final step: compress files and delete temp folder
+cd /home/builder/output/ && tar czvf blazingsql-files.tar.gz blazingsql-files/
+rm -rf ${output}
+
+#END collect/package artifacts
 
 cd $working_directory
 
@@ -395,54 +435,6 @@ cd $working_directory
 exit
 
 
-# this function build the stack
-function build_blazingsql() {
-	
-
-
-
-    cd ${workspace}
-
-    # cudf
-    if [ -d cudf ]; then
-    fi
-
-    git clone git@github.com:BlazingDB/cudf.git
-    cd cudf
-    git checkout develop
-
-    # blazingdb-protocol
-    git clone git@github.com:BlazingDB/blazingdb-protocol.git
-    cd ${workspace}/blazingdb-protocol && git checkout develop
-    cd ${workspace}/blazingdb-protocol/java && mvn clean install
-
-    # blazingdb-ral
-    cd ${workspace}
-    git clone git@github.com:BlazingDB/blazingdb-ral.git
-    cd  ${workspace}/blazingdb-ral && git checkout develop
-    mkdir ${workspace}/blazingdb-ral/build && cd ${workspace}/blazingdb-ral/build
-    cmake .. && make
-
-    # blazingdb-orchestrator
-    cd ${workspace}
-    git clone git@github.com:BlazingDB/blazingdb-orchestrator.git
-    cd  ${workspace}/blazingdb-orchestrator && git checkout develop
-    mkdir ${workspace}/blazingdb-orchestrator/build && cd ${workspace}/blazingdb-orchestrator/build
-    cmake .. && make -j8
-
-    # blazingdb-calcite
-    cd ${workspace}
-    git clone git@github.com:BlazingDB/blazingdb-calcite.git
-    cd ${workspace}/blazingdb-calcite && git checkout develop
-    mvn clean install -Dmaven.test.skip=true
-
-    # PyBlazing
-    cd ${workspace}
-    git clone git@github.com:BlazingDB/pyBlazing.git
-    cd ${workspace}/pyBlazing && git checkout develop
-    
-    
-}
 
 function zip_cpp_project() {
     workspace=$1
@@ -462,49 +454,7 @@ function zip_cpp_project() {
 
 # this function just read the content of /home/builder/src and copy the binary files
 function zip_files() {
-    workspace=/home/builder/src
-    output=/home/builder/output/blazingsql-files
 
-    mkdir -p ${output}/libgdf_cffi
-    mkdir -p ${output}/blazingdb-protocol/python/
-
-    # Package blazingdb-ral
-    zip_cpp_project $workspace $output "blazingdb-ral" "testing-libgdf"
-
-    #TODO fix cmake files, build first libgdf and bz-protocol then pass the paths
-    # Package libgdf and libgdf_cffi from blazingdb-ral
-    build_directory="build"
-    if [ -f $workspace/blazingdb-ral/CMakeFiles/thirdparty/libgdf-install/lib/libgdf.so ]; then
-        build_directory=""
-    fi
-    cp -r $workspace/blazingdb-ral/$build_directory/CMakeFiles/thirdparty/libgdf-src/libgdf/python/* $output/libgdf_cffi/
-    cp -r $workspace/blazingdb-ral/$build_directory/CMakeFiles/thirdparty/libgdf-install/* $output/libgdf_cffi/
-    cp -r $workspace/blazingdb-ral/$build_directory/CMakeFiles/thirdparty/libgdf-src/libgdf/include/*.h $output/libgdf_cffi/include/
-    rm -rf $output/libgdf_cffi/lib/libgdf.a
-
-    # Package blazingdb-orchestrator
-    zip_cpp_project $workspace $output "blazingdb-orchestrator" "blazingdb_orchestator_service"
-
-    # Package blazingdb-calcite
-    cp $workspace/blazingdb-calcite/blazingdb-calcite-application/target/BlazingCalcite.jar ${output}
-
-    # Package blazingdb-protocol/python
-    cp -r $workspace/blazingdb-protocol/python/* $output/blazingdb-protocol/python/
-
-    # Package PyBlazing
-    echo "### PyBlazing ###"
-    cp -r $workspace/pyBlazing/ ${output}/
-    rm -rf ${output}/pyBlazing/.git/
-
-    # Cudf
-    echo "### Cudf ###"
-    mkdir -p ${workspace}/cudf/conda-recipes/cudf/ && \
-    cp -r ${workspace}/cudf/ ${output}/
-    rm -rf ${output}/cudf/.git/
-
-    # compress files and delete temp folder
-    cd /home/builder/output/ && tar czvf blazingsql-files.tar.gz blazingsql-files/
-    rm -rf ${output}
 }
 
 #BEGIN MAIN

120139a1c08a7e9f133eb8736a9167c3d2c4b0c2
mario21ic <Mario Inga Cahuana>
2018-11-28 18:06:43 -0500

    fix(ci-calcite) actualizando script con uid y gid

--- docker/ci-blazingdb-calcite/docker-run.sh
+++ docker/ci-blazingdb-calcite/docker-run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-nvidia-docker run --rm -v docker/ci-blazingdb-calcite/src_calcite:/home/builder/workspace -v $HOME/.ssh/:/home/builder/.ssh/ -v /$HOME/.m2/:/home/builder/.m2 blazingdb/calcite
+nvidia-docker run --user $(id -u):$(id -g) --rm -v $PWD/src_calcite:/home/builder/workspace -v $HOME/.ssh/:/home/builder/.ssh/ -v /$HOME/.m2/:/home/builder/.m2 blazingdb/calcite
 

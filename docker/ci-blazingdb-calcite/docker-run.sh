4f83d1e4c3d33bf9f85ad8749558aaa3d2385c47
editaxz <edit44@outlook.com>
2018-11-28 15:40:00 -0500

    add docker run

--- docker/ci-blazingdb-calcite/docker-run.sh
+++ docker/ci-blazingdb-calcite/docker-run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-nvidia-docker run --rm -v $PWD:/home/builder/workspace -v $HOME/.ssh/:/home/builder/.ssh/ -v /$HOME/.m2/:/home/builder/.m2 blazingdb/calcite
+nvidia-docker run --rm -v docker/ci-blazingdb-calcite/src_calcite:/home/builder/workspace -v $HOME/.ssh/:/home/builder/.ssh/ -v /$HOME/.m2/:/home/builder/.m2 blazingdb/calcite
 

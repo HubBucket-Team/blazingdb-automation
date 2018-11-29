c4a2a261ab77908fa69d3c5d65069d8ea9835f9d
editaxz <edit44@outlook.com>
2018-11-29 10:25:20 -0500

    fix m2 repository volumen

--- docker/ci-blazingdb-protocol/docker-run.sh
+++ docker/ci-blazingdb-protocol/docker-run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-nvidia-docker run --rm -v $WORKSPACE/docker/ci-blazingdb-protocol/src_protocol:/home/builder/workspace -v $HOME/.ssh/:/home/builder/.ssh/ -v /$HOME/.m2/:/home/builder/.m2 blazingdb/protocol
+nvidia-docker run --rm -v $WORKSPACE/docker/ci-blazingdb-protocol/src_protocol:/home/builder/workspace -v $HOME/.ssh/:/home/builder/.ssh/ -v $HOME/.m2/:/home/builder/.m2 blazingdb/protocol

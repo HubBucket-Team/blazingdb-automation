83c5ba9c427da94ffdc1e7717980941fbdf52b59
mario21ic <Mario Inga Cahuana>
2018-11-29 15:16:07 -0500

    feat(jenkins) agregando Jenkinsfile

--- docker/build-deploy.sh
+++ docker/build-deploy.sh
@@ -1,35 +1,36 @@
 #!/bin/bash
 
 #BUILD
-cd blazingsql-build
-
-mkdir -R $HOME/blazingsql/workspace/
-mkdir -R $HOME/blazingsql/output/
+cd blazingsql-build/
 
 workspace=$HOME/blazingsql/workspace/
 output=$HOME/blazingsql/output/
+image_build="blazingsql/build:$1"
+image_deploy="blazingsql/deploy:$1"
 
-chown 1000:1000 -R $HOME/workspace
-chown 1000:1000 -R $HOME/output
-chown 1000:1000 -R $HOME/.ssh
+mkdir -p $workspace $output
 
-cp blazingsql-build.properties $HOME/workspace/
+sudo chown 1000:1000 -R $workspace
+sudo chown 1000:1000 -R $ouput
+sudo chown 1000:1000 -R $HOME/.ssh
 
-nvidia-docker build -t test .
-
-nvidia-docker run --user 1000:1000 --rm -v $workspace/:/home/builder/workspace/ -v $output/:/home/builder/output -v $HOME/.ssh/:/home/builder/.ssh/ 
-
-cp $output/blazingsql-files.tar.gz blazingsql/
+cp blazingsql-build.properties $workspace
 
 
-#BEFORE DEPLOY
-cd blazingsql
+nvidia-docker build -t $image_build .
+# User builder uid=1000, but user jenkins uid=123
+nvidia-docker run --user 1000:1000 --rm -v $workspace:/home/builder/workspace/ -v $output:/home/builder/output -v $HOME/.ssh/:/home/builder/.ssh/ $image_build
+
+cp $output/blazingsql-files.tar.gz ./blazingsql/
+
+
+# BEFORE DEPLOY
+cd blazingsql 
+
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
 chmod +x Miniconda3-latest-Linux-x86_64.sh
 
 #DEPLOY
-nvidia-docker build -t deploytest .
+nvidia-docker build -t $image_deploy .
+nvidia-docker run --rm -d -p 8884:8888 -p 8787:8787 -p 8786:8786 -p 9001:9001 $image_deploy
 
-nvidia-docker run --rm -p 8884:8888 -p 8787:8787 -p 8786:8786 -p 9001:9001 deploytest
-
-#JOB PUBLISH

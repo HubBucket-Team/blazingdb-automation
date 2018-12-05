75b640ff14f1ab9eefcd52ada6ea2deb05ddea77
editaxz <edit44@outlook.com>
2018-12-04 22:48:41 -0500

    protocol

--- docker/ci-blazingdb-protocol/docker-run.sh
+++ docker/ci-blazingdb-protocol/docker-run.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-nvidia-docker run --user $(id -u):$(id -g)  --rm -v $WORKSPACE/docker/ci-blazingdb-protocol/src_protocol:/home/builder/workspace -v $HOME/.ssh/:/home/builder/.ssh/ -v $HOME/.m2/:/home/builder/.m2 blazingdb/protocol
+#nvidia-docker run --user $(id -u):$(id -g) -v $WORKSPACE/docker/ci-blazingdb-protocol/src_protocol:/home/builder/workspace -v $HOME/.ssh/:/home/builder/.ssh/ -v $HOME/.m2/:/home/builder/.m2 blazingdb/protocol
+nvidia-docker run --user 1000:1000 --rm -v $WORKSPACE/docker/ci-blazingdb-protocol/src_protocol:/home/builder/workspace -v $HOME/.ssh/:/home/builder/.ssh/ -v $HOME/.m2/:/home/builder/.m2 blazingdb/protocol

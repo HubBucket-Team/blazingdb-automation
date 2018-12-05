39359351479b62e5616f2c4c6bdee18437646d78
editaxz <edit44@outlook.com>
2018-12-04 21:35:53 -0500

    calcite ci

--- docker/ci-blazingdb-calcite/docker-run.sh
+++ docker/ci-blazingdb-calcite/docker-run.sh
@@ -2,4 +2,4 @@
 # Usage:   ./docker-run.sh dir_src dir_ssh dir_m2
 # Example: ./docker-run.sh src_calcite_develop/ $HOME/.ssh/ $HOME/.m2/
 
-nvidia-docker run --user $(id -u):$(id -g)  --rm -v $WORKSPACE/docker/ci-blazingdb-protocol/src_protocol:/home/builder/workspace -v $HOME/.ssh/:/home/builder/.ssh/ -v $HOME/.m2/:/home/builder/.m2 blazingdb/calcite
+nvidia-docker run --user $(id -u):$(id -g)  --rm -v $WORKSPACE/docker/ci-blazingdb-calcite/src_calcite:/home/builder/workspace -v $HOME/.ssh/:/home/builder/.ssh/ -v $HOME/.m2/:/home/builder/.m2 blazingdb/calcite

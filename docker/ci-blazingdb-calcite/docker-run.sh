6db0e04cb908d0488caa3a53cfa1edc70e7a3203
editaxz <edit44@outlook.com>
2018-12-04 21:09:20 -0500

    add ci to calcite

--- docker/ci-blazingdb-calcite/docker-run.sh
+++ docker/ci-blazingdb-calcite/docker-run.sh
@@ -2,5 +2,4 @@
 # Usage:   ./docker-run.sh dir_src dir_ssh dir_m2
 # Example: ./docker-run.sh src_calcite_develop/ $HOME/.ssh/ $HOME/.m2/
 
-nvidia-docker run --user $(id -u):$(id -g) --rm -v $1:/home/builder/workspace -v $2:/home/builder/.ssh/ -v $3:/home/builder/.m2 blazingdb/calcite
-
+nvidia-docker run --user $(id -u):$(id -g)  --rm -v $WORKSPACE/docker/ci-blazingdb-protocol/src_protocol:/home/builder/workspace -v $HOME/.ssh/:/home/builder/.ssh/ -v $HOME/.m2/:/home/builder/.m2 blazingdb/calcite

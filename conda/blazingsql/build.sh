25b44b68e9ec5a61baeb9b856f9af93a46c4e477
mario21ic <Mario Inga Cahuana>
2018-12-06 18:56:37 -0500

    feat(conda) adding support to blazingsql

--- conda/blazingsql/build.sh
+++ conda/blazingsql/build.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 
-$PYTHON setup.py install
+echo "### Building from build.sh ###"
 
+DIR=$PWD
+WORKDIR=/tmp/blazingsql
+
+#tar -xvf $DIR/blazingsql.tar.gz -C $DIR/
+#cp mypackapage.tar.gz /tmp/
+
+mkdir -p $WORKDIR
+cp $DIR/BlazingCalcite.jar $WORKDIR/
+cp $DIR/blazingdb_orchestator_service $WORKDIR/
+cp $DIR/testing-libgdf $WORKDIR/
+
+#cd $DIR/blazingdb-protocol/python/ && python setup.py install
+#cd $DIR/pyBlazing/ && python setup.py install

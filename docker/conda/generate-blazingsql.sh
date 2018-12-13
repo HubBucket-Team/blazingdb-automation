9d5751e4952ee5467cc0fced709c160a7639a080
Percy Camilo Triveño Aucahuasi <percy@blazingdb.com>
2018-12-13 08:06:55 -0500

    Create the docker env to generate the blazingsql conda package

--- docker/conda/generate-blazingsql.sh
+++ docker/conda/generate-blazingsql.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 blazingsql_files_tar_gz_path=$1
+output_dir=$2
+
+source activate cudf
+
+blazingsql_dir=$output_dir/blazingsql
 
 working_directory=$PWD
 
@@ -14,25 +19,30 @@ mkdir -p $working_space
 
 echo "Decompressing blazingsql-files.tar.gz ..."
 # TODO percy uncomment this when finish this script
-#tar xf $blazingsql_files_tar_gz_path -C $working_space
+tar xf $blazingsql_files_tar_gz_path -C $working_space
 echo "blazingsql-files.tar.gz was decompressed at $working_space"
 
 # Creating the blazingsql python package
-mkdir -p blazingsql/bin blazingsql/pyblazing blazingsql/runtime
-cp -r blazingsql-template/* blazingsql
+mkdir -p $blazingsql_dir/bin
+mkdir -p $blazingsql_dir/pyblazing
+mkdir -p $blazingsql_dir/runtime
+
+cp -r blazingsql-template/* $blazingsql_dir
 
 blazingdb_ral_artifact_name=testing-libgdf
 blazingdb_orchestrator_artifact_name=blazingdb_orchestator_service
 blazingdb_calcite_artifact_name=BlazingCalcite.jar
 
-cp $blazingsql_files_dir/$blazingdb_ral_artifact_name blazingsql/bin
-cp $blazingsql_files_dir/$blazingdb_orchestrator_artifact_name blazingsql/bin
-cp $blazingsql_files_dir/$blazingdb_calcite_artifact_name blazingsql/bin
+cp $blazingsql_files_dir/$blazingdb_ral_artifact_name $blazingsql_dir/bin
+cp $blazingsql_files_dir/$blazingdb_orchestrator_artifact_name $blazingsql_dir/bin
+cp $blazingsql_files_dir/$blazingdb_calcite_artifact_name $blazingsql_dir/bin
 
-cp -r $blazingsql_files_dir/blazingdb-protocol/python/blazingdb/* blazingsql/blazingdb
-cp -r $blazingsql_files_dir/pyBlazing/pyblazing/* blazingsql/pyblazing
+cp -r $blazingsql_files_dir/blazingdb-protocol/python/blazingdb/* $blazingsql_dir/blazingdb
+cp -r $blazingsql_files_dir/pyBlazing/pyblazing/* $blazingsql_dir/pyblazing
 
 cd $blazingsql_files_dir/cudf/python
-python setup.py install --prefix $working_directory/blazingsql/runtime
+python setup.py build_ext --inplace
+pip install .
+# --prefix $blazingsql_dir/runtime
 
 cd $working_directory

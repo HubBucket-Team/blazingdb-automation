6d71e9de7f6786fa0eb6d469baad646504c2f120
Percy Camilo Triveño Aucahuasi <percy@blazingdb.com>
2018-12-13 01:46:18 -0500

    Improve blazingsql conda package generation

--- conda/generate-blazingsql.sh
+++ conda/generate-blazingsql.sh
@@ -2,10 +2,44 @@
 
 blazingsql_files_tar_gz_path=$1
 
-temp_directory=/tmp
-
 working_directory=$PWD
+
+temp_dir=/tmp
+blazingsql_files_dir_name=blazingsql-files
+working_space_name=_safe_to_remove_
+working_space=/tmp/"$blazingsql_files_dir_name$working_space_name"
+blazingsql_files_dir=$working_space/$blazingsql_files_dir_name
+
+mkdir -p $working_space
+
+echo "Decompressing blazingsql-files.tar.gz ..."
+# TODO percy uncomment this when finish this script
+#tar xf $blazingsql_files_tar_gz_path -C $working_space
+echo "blazingsql-files.tar.gz was decompressed at $working_space"
+
+# Creating the blazingsql python package
+mkdir -p blazingsql/bin blazingsql/pyblazing
+cp -r blazingsql-template/* blazingsql
+
+blazingdb_ral_artifact_name=testing-libgdf
+blazingdb_orchestrator_artifact_name=blazingdb_orchestator_service
+blazingdb_calcite_artifact_name=BlazingCalcite.jar
+
+cp $blazingsql_files_dir/$blazingdb_ral_artifact_name blazingsql/bin
+cp $blazingsql_files_dir/$blazingdb_orchestrator_artifact_name blazingsql/bin
+cp $blazingsql_files_dir/$blazingdb_calcite_artifact_name blazingsql/bin
+
+cp -r $blazingsql_files_dir/blazingdb-protocol/python/blazingdb/* blazingsql/blazingdb
+cp -r $blazingsql_files_dir/pyBlazing/pyblazing/* blazingsql/pyblazing
+
 cd $working_directory
+
+exit
+
+
+
+
+
 
 
 # Define binaries path
@@ -30,4 +64,5 @@ cp -r $pyblazing_current_dir/pyBlazing/* ${conda_workspace}/pyBlazing/
 
 # Generate the packages
 cd ${workspace_package} && tar czf blazingsql.tar.gz blazingsql/
+
 

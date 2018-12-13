4acbfc25d77fb519bfc220c0c0c777f74a99329d
Percy Camilo Triveño Aucahuasi <percy@blazingdb.com>
2018-12-13 11:26:58 -0500

    Improve deploy docker image so we support conda env for any user

--- docker/blazingsql/supervisord/run_jupyter.sh
+++ docker/blazingsql/supervisord/run_jupyter.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-source activate cudf && jupyter-lab --allow-root --ip=0.0.0.0 --no-browser --NotebookApp.token='rapids'
+source activate cudf && jupyter-lab --notebook=/blazingdb/ --allow-root --ip=0.0.0.0 --no-browser --NotebookApp.token='rapids'

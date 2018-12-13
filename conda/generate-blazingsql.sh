4d8f2a8296962bc16a569ebfb1128f9f5bd486f0
Percy Camilo Triveño Aucahuasi <percy@blazingdb.com>
2018-12-13 02:02:48 -0500

    Start think about how to manage custom cudf runtime

--- conda/generate-blazingsql.sh
+++ conda/generate-blazingsql.sh
@@ -18,7 +18,7 @@ echo "Decompressing blazingsql-files.tar.gz ..."
 echo "blazingsql-files.tar.gz was decompressed at $working_space"
 
 # Creating the blazingsql python package
-mkdir -p blazingsql/bin blazingsql/pyblazing
+mkdir -p blazingsql/bin blazingsql/pyblazing blazingsql/runtime
 cp -r blazingsql-template/* blazingsql
 
 blazingdb_ral_artifact_name=testing-libgdf
@@ -32,37 +32,7 @@ cp $blazingsql_files_dir/$blazingdb_calcite_artifact_name blazingsql/bin
 cp -r $blazingsql_files_dir/blazingdb-protocol/python/blazingdb/* blazingsql/blazingdb
 cp -r $blazingsql_files_dir/pyBlazing/pyblazing/* blazingsql/pyblazing
 
+cd $blazingsql_files_dir/cudf/python
+python setup.py install --prefix $working_directory/blazingsql/runtime
+
 cd $working_directory
-
-exit
-
-
-
-
-
-
-
-# Define binaries path
-blazingdb_ral_artifact_name=$workspace_dir/testing-libgdf
-blazingdb_calcite_artifact_name=$workspace_dir/BlazingCalcite.jar
-blazingdb_orchestrator_artifact_name=$workspace_dir/blazingdb_orchestator_service
-blazingdb_protocol_current_dir=$workspace_dir
-pyblazing_current_dir=$workspace_dir
-
-# Create basic folders
-conda_workspace=${workspace_package}/blazingsql
-mkdir -p ${conda_workspace}/blazingdb-protocol/python/
-mkdir -p ${conda_workspace}/pyBlazing/
-#mkdir -p ${conda_workspace}/blazingsql/
-
-# Copy the binaries into package
-cp $blazingdb_ral_artifact_name ${conda_workspace}/
-cp $blazingdb_calcite_artifact_name ${conda_workspace}/
-cp $blazingdb_orchestrator_artifact_name ${conda_workspace}/
-cp -r $blazingdb_protocol_current_dir/blazingdb-protocol/python/* ${conda_workspace}/blazingdb-protocol/python/
-cp -r $pyblazing_current_dir/pyBlazing/* ${conda_workspace}/pyBlazing/
-
-# Generate the packages
-cd ${workspace_package} && tar czf blazingsql.tar.gz blazingsql/
-
-

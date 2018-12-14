b6b07670eb91f0389ac80f0fa643ccd3031bfcb2
Percy Camilo Triveño Aucahuasi <percy@blazingdb.com>
2018-12-14 03:11:08 -0500

    Implement a simple python plugin to run pre installation steps

--- docker/conda/blazingsql-template/setup.py
+++ docker/conda/blazingsql-template/setup.py
@@ -1,7 +1,14 @@
 # Copyright (c) 2018, BlazingDB
 
 from setuptools import setup, find_packages
+from setuptools.command.install import install
 import os
+
+class MyInstall(install):
+
+    def run(self):
+        install.run(self)
+        print("X\n\n\n\nI did it!!!!\n\n\n\nZ")
 
 os.system("echo 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA'")
 
@@ -13,6 +20,7 @@ setup(
     author_email='blazing@blazingdb',
     packages=find_packages(include=['blazingsql', 'blazingsql.*']),
     install_requires=['flatbuffers'],
+    cmdclass={'install': MyInstall},
     zip_safe=False
 )
 

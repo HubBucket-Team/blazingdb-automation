b4e4f2852282756fac4b38aa67ab7f54670e358f
gcca <cristhian@blazingdb.com>
2018-12-19 09:18:53 -0500

    [sprint13-conda] Update paths for blazingsql runtime environment

--- docker/conda/blazingsql-template/blazingsql/__init__.py
+++ docker/conda/blazingsql-template/blazingsql/__init__.py
@@ -1,12 +1,17 @@
 import os
 
-BLAZINGSQL_DIR = os.dirname(__file__)
+BLAZINGSQL_DIR = os.path.dirname(__file__)
 RUNTIME_DIR = os.path.join(BLAZINGSQL_DIR, 'runtime')
-
-os.environ['LD_LIBRARY_PATH'] = RUNTIME_DIR
+LIB_DIR = os.path.join(RUNTIME_DIR, 'lib')
+SITE_DIR = os.path.join(LIB_DIR, 'python3.5', 'site-packages')
 
 import sys
-sys.path.insert(0, RUNTIME_DIR)
+sys.path.insert(0, SITE_DIR)
+
+import ctypes
+ctypes.cdll.LoadLibrary(os.path.join(LIB_DIR, 'librmm.so'))
+ctypes.cdll.LoadLibrary(os.path.join(LIB_DIR, 'libNVStrings.so'))
+ctypes.cdll.LoadLibrary(os.path.join(LIB_DIR, 'libcudf.so'))
 
 import importlib
 import cudf

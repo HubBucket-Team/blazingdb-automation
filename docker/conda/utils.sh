902d33639fbbfec7d9d2c377d4ce73dc01cb3369
editaxz <edit44@outlook.com>
2018-12-20 00:08:31 -0500

    update utils print home

--- docker/conda/utils.sh
+++ docker/conda/utils.sh
@@ -15,4 +15,6 @@ chmod +x Miniconda3-latest-Linux-x86_64.sh
 output=$HOME/blazingsql/conda_output/
 sudo chown 1000:1000 -R $output
 
+echo "HOME/blazingsql/conda_output/ ====>>>> " $HOME/blazingsql/conda_output/
+
 

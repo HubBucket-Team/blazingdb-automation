9287605b7c0b9eb9fedc6d770790edf32b6dbecf
mario21ic <Mario Inga Cahuana>
2018-12-20 17:55:24 -0500

    feat(conda) agregando script to upload & echos

--- docker/conda/conda_run_upload.sh
+++ docker/conda/conda_run_upload.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # Usage: input_path label
 
-nvidia-docker run --user 1000:1000 --rm -v $1:/tmp/ -v $PWD/conda_upload.sh:/tmp/conda_upload.sh blazingdb/conda /tmp/conda_upload.sh $1 $2
+echo "nvidia-docker run --user 1000:1000 --rm -v $1:/tmp/tar/ -v $PWD/conda_upload.sh:/tmp/conda_upload.sh blazingdb/conda /tmp/conda_upload.sh $1 $2"
+nvidia-docker run --user 1000:1000 --rm -v $1:/tmp/tar/ -v $PWD/conda_upload.sh:/tmp/conda_upload.sh blazingdb/conda /tmp/conda_upload.sh $1 $2
--- docker/conda/conda_upload.sh
+++ docker/conda/conda_upload.sh
@@ -3,4 +3,5 @@
 
 source activate builder
 anaconda login --username mario21ic --password blazingdb2018123
+echo "anaconda upload --user BlazingDB $1 --label $2"
 anaconda upload --user BlazingDB $1 --label $2

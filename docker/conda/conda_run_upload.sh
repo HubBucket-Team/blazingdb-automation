63ac0c666385f30355b4ce9611188f431ac52c0f
mario21ic <Mario Inga Cahuana>
2018-12-20 18:05:36 -0500

    feat(conda) agregando params para el script

--- docker/conda/conda_run_upload.sh
+++ docker/conda/conda_run_upload.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Usage: input_path label
 
-echo "nvidia-docker run --user 1000:1000 --rm -v $1:/tmp/tar/ -v $PWD/conda_upload.sh:/tmp/conda_upload.sh blazingdb/conda /tmp/conda_upload.sh $1 $2"
-nvidia-docker run --user 1000:1000 --rm -v $1:/tmp/tar/ -v $PWD/conda_upload.sh:/tmp/conda_upload.sh blazingdb/conda /tmp/conda_upload.sh $1 $2
+echo "nvidia-docker run --user 1000:1000 --rm -v $1:/tmp/$2 -v $PWD/conda_upload.sh:/tmp/conda_upload.sh blazingdb/conda /tmp/conda_upload.sh /tmp/$2 $3"
+nvidia-docker run --user 1000:1000 --rm -v $1:/tmp/$2 -v $PWD/conda_upload.sh:/tmp/conda_upload.sh blazingdb/conda /tmp/conda_upload.sh /tmp/$2 $3

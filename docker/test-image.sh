4a187303c4026ed84baa8ad31e923daa73916ad5
mario21ic <Mario Inga Cahuana>
2019-01-21 16:15:58 -0500

    feat(docker) fixeando las rutas

--- docker/test-image.sh
+++ docker/test-image.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-# Usage: tag_deploy
+# Project https://github.com/GoogleContainerTools/container-structure-test
+# Usage: ./test-image.sh tag_deploy
 
 container-structure-test test --image blazingdb/blazingsql:$1 --config container-structure-test.yaml

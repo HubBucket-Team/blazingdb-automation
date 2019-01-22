a296c42d5019c4ae549784daed161f373b185925
mario21ic <Mario Inga Cahuana>
2019-01-22 16:45:22 -0500

    feat(publish) agregando image param para test docker image

--- docker/test-image.sh
+++ docker/test-image.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Project https://github.com/GoogleContainerTools/container-structure-test
-# Usage: ./test-image.sh tag_deploy
+# Usage: ./test-image.sh docker_image
 
-container-structure-test test --image blazingdb/blazingsql:$1 --config container-structure-test.yaml
+container-structure-test test --image $1 --config container-structure-test.yaml

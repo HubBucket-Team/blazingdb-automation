5ee5ab33e1e599508203e7236d2e2c82209d74e1
edith <edit44@outlook.com>
2019-01-23 15:55:30 -0500

    test upload

--- docker/conda/conda_run.sh
+++ docker/conda/conda_run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # Usage: /path/input/files /path/output/files
-nvidia-docker run  --user 1000:1000 --rm -v $1:/home/jupyter/input -v $2:/home/jupyter/output blazingdb/conda
+nvidia-docker run  --user 1000:1000 --rm -v $1:/home/jupyter/input -v $2:/home/jupyter/output blazingdb/conda /home/jupyter/generate-blazingsql.sh /home/jupyter/input/blazingsql-files.tar.gz /home/jupyter/output 2.2 2
+ 

af851c84ecb3fe12570519413f569c45e326eccd
edith <edit44@outlook.com>
2019-01-22 19:03:56 -0500

    add version to package on cloud

--- docker/conda/utils.sh
+++ docker/conda/utils.sh
@@ -6,7 +6,8 @@ version=$2
 python_version=$3 
 build_number=$4
 
-mv ${OUTPUT}/$file_name blazingsql-${version}-${python_version}_${build_number}
+mv $file_name blazingsql-${version}-${python_version}_${build_number}
+ls $file_name 
 
 #1.0
 #py35

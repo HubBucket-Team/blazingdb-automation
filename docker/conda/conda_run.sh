57a29a0321b0169f5b3d9df205d1ca8e897b056b
edith <edit44@outlook.com>
2019-01-23 17:47:47 -0500

    clean the workspace conda

--- docker/conda/conda_run.sh
+++ docker/conda/conda_run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Usage: /path/input/files /path/output/files
-nvidia-docker run  --user 1000:1000 --rm -v $1:/home/jupyter/input -v $2:/home/jupyter/output blazingdb/conda /home/jupyter/generate-blazingsql.sh /home/jupyter/input/blazingsql-files.tar.gz /home/jupyter/output 4.4 4 py35 
+nvidia-docker run  --user 1000:1000 --rm -v $1:/home/jupyter/input -v $2:/home/jupyter/output blazingdb/conda /home/jupyter/generate-blazingsql.sh /home/jupyter/input/blazingsql-files.tar.gz /home/jupyter/output 5.5 5 py35 
  

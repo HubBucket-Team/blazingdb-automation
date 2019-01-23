202062df6b868dacaf21e911c4a2bb4330a0b4e4
edith <edit44@outlook.com>
2019-01-23 17:27:44 -0500

    add parameter to copy package

--- docker/conda/generate-blazingsql.sh
+++ docker/conda/generate-blazingsql.sh
@@ -93,7 +93,7 @@ echo "BUILD_NUMBERR: $4"
 
 echo "COPYY"
 
-cp /tmp/blazingsql-files_conda_safe_to_remove_/linux-64/blazingsql-*.tar.bz2  /home/jupyter/output/
+cp /tmp/blazingsql-files_conda_safe_to_remove_/linux-64/blazingsql-$3-$5_$4.tar.bz2  /home/jupyter/output/
 
 
 

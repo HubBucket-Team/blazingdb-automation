a1194bd5063cd086ae672dfc665370f703df5f92
editaxz <edit44@outlook.com>
2019-01-24 07:40:15 -0500

    add parameter label

--- docker/conda/conda_upload.sh
+++ docker/conda/conda_upload.sh
@@ -4,4 +4,4 @@
 source activate user
 anaconda login --username mario21ic --password blazingdb2018123
 echo "anaconda upload --user BlazingDB $1 --label $2"
-anaconda upload --user BlazingDB $1 -l $2
+anaconda upload --user BlazingDB $1 --label $2

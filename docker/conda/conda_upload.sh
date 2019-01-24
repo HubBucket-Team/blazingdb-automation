1d21d0a4b1bb5ef5980feb7ac8c8a31d3d3ed576
edith <edit44@outlook.com>
2019-01-24 11:51:13 -0500

    add test labesl

--- docker/conda/conda_upload.sh
+++ docker/conda/conda_upload.sh
@@ -4,7 +4,7 @@
 source activate user
 anaconda login --username mario21ic --password blazingdb2018123
 echo "anaconda upload --user BlazingDB $1 --label $2"
-anaconda upload --user BlazingDB $1 --label $2
+anaconda upload --user BlazingDB $1 --label main --label cuda9.5 --label dev --lavel prod
 
 
 echo "PRINT LABELSSS ==> " $2

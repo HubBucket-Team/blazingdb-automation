66aa6ed275b877e3f23f29780fa1c1844fec107b
editaxz <edit44@outlook.com>
2019-02-04 15:27:49 -0500

    parameterized directories

--- docker/blazingsql-testing/run_complete_test.sh
+++ docker/blazingsql-testing/run_complete_test.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-#Clean 
+# Remove old containers
+echo "Removing old containers"
 docker stop $(docker ps -a -q)
 docker rm $(docker ps -a -q)
 
-#Build image e2etest
-workdir=/home/edith/blazingdb/workspace-testing
+# Set directories workspace
+home_user=/home/edith/blazingdb
+workdir=$home_user/workspace-testing
 workdir_testing=$workdir/blazingdb-testing
-workdir_scriptdrill=/home/edith/blazingdb/repositories/blazingsql/blazingdb-automation/docker/blazingsql-testing
-workdir_drill=/home/edith/blazingdb/apache-drill-1.12.0/bin/drill-embedded
+workdir_scriptdrill=$home_user/repositories/blazingsql/blazingdb-automation/docker/blazingsql-testing
+workdir_drill=$home_user/apache-drill-1.12.0
 
-# chown -R  edith:edith  $workdir
-
-# Build e2e image
+# Build end to end test image
 echo "Building e2e test image"
-#./e2e_build.sh
 nvidia-docker build -t blazingsqltest .
 
 # Repository  blazingdb-testing
+echo "Updading blazingdb-testing repository"
 cd $workdir
 blazingdb_testing_name=blazingdb-testing
 
@@ -27,23 +27,25 @@ echo " Clonning blazingdb-testing"
     cd $workdir_testing
     git checkout develop
 fi 
-
 cd $workdir_testing
 git checkout develop
 git pull
 
 # Install apache drill
-echo " Install apache drill"
+echo " Installig apache drill"
 cd $workdir
-apache_drill_name=apache-drill-1.12.0
+apache_drill_directory=apache-drill-1.12.0
 
-if [ ! -d $apache_drill_name ]; then
+if [ ! -d $apache_drill_directory ]; then
     wget http://archive.apache.org/dist/drill/drill-1.12.0/apache-drill-1.12.0.tar.gz
     tar -xvzf apache-drill-1.12.0.tar.gz
-fi   
-# Copy Data MB
+fi 
+# TODO: Set time zone apacje drill:  In the folder : /apache-drill-1.12.0/conf/ edit the file drill-env.sh and add the line: export DRILL_JAVA_OPTS="-Duser.timezone=UTC"
 
-# Create LogData
+# TODO: Download DataSet1Mb
+
+# Create logtest
+echo "Updating creation logtest directory "
 logTest_name=logtest
 
 if [ ! -d $logTest_name ]; then
@@ -51,16 +53,15 @@ if [ ! -d $logTest_name ]; then
 fi   
 
 # Executing container e2e
-echo "Run e2e test image"
-#./e2e_run.sh
-nvidia-docker run --name bzsqlcontainer -d -p 8888:8888 -p 8887:8787 -p 8886:8786 -p 9002:9001  -v $workdir_scriptdrill/run_e2e.sh:/tmp/run_e2e.sh -v /home/edith/blazingdb/workspace-testing/:/home/edith/blazingdb  -ti blazingsqltest  bash
+echo "Run end to end  test container"
+nvidia-docker run --name bzsqlcontainer -d -p 8888:8888 -p 8887:8787 -p 8886:8786 -p 9002:9001  -v $workdir_scriptdrill/run_e2e.sh:/tmp/run_e2e.sh -v $workdir/:$home_user -ti blazingsqltest  bash
 
 
 # Change permissions
 echo "Changing permission"
 nvidia-docker exec -u root bzsqlcontainer chown -R edith:edith /blazingsql/
-nvidia-docker exec -u root bzsqlcontainer chown -R edith:edith /home/edith/blazingdb/
-nvidia-docker exec -u root bzsqlcontainer chown -R edith:edith /home/edith/blazingdb/apache-drill-1.12.0/
+nvidia-docker exec -u root bzsqlcontainer chown -R edith:edith $home_user
+nvidia-docker exec -u root bzsqlcontainer chown -R edith:edith $workdir_drill
 
 
 # Init services
@@ -71,11 +72,9 @@ nvidia-docker exec -d bzsqlcontainer java -jar  /home/jupyter/BlazingCalcite.jar
 
 # Init apache Drill
 echo "Init apache Drill"
-#nvidia-docker exec -d bzsqlcontainer   /home/edith/blazingdb/apache-drill-1.12.0/bin/drill-embedded
 #!quit
 cd $workdir_scriptdrill
-./run_drill.sh  $workdir_drill
-
+./run_drill.sh  $workdir_drill/bin/drill-embedded
 sleep 10
 
 # Init e2e test
--- docker/blazingsql-testing/run_drill.sh
+++ docker/blazingsql-testing/run_drill.sh
@@ -1,11 +1,2 @@
 #!/bin/bash
-#nvidia-docker exec -it bzsqlcontainer /bin/bash
-#cd /home/edith/blazingdb/apache-drill-1.12.0/bin/
-#./drill-embedded
-
-#OLD
-#nvidia-docker exec -it bzsqlcontainer  /home/edith/blazingdb/apache-drill-1.12.0/bin/drill-embedded
-
-
-#NEW
 nvidia-docker exec -it -d bzsqlcontainer $1

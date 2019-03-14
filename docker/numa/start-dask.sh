af5fe12619cb617e6b4f0259c084af15bc76990d
mario21ic <Mario Inga Cahuana>
2019-03-14 16:24:53 -0500

    feat(dask) agregando 2 workers

--- docker/numa/start-dask.sh
+++ docker/numa/start-dask.sh
@@ -6,4 +6,5 @@ docker build -t blazingdb/dask:worker ./dask-worker/
 docker network create --subnet=172.18.0.0/16 dask_net
 docker run --net dask_net --ip 172.18.0.22 -p 8787 -d blazingdb/dask:scheduler
 docker run --net dask_net --ip 172.18.0.23 -d blazingdb/dask:worker
-docker run --net dask_net --ip 172.18.0.23 -d blazingdb/dask:worker
+docker run --net dask_net --ip 172.18.0.24 -d blazingdb/dask:worker
+docker run --net dask_net --ip 172.18.0.25 -d blazingdb/dask:worker

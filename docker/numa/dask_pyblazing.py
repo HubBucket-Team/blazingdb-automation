5f72e97ec5d7a926625df7eaed945d355f916bcf
mario21ic <Mario Inga Cahuana>
2019-03-14 01:18:15 -0500

    refact(dask) mejorando carga de pyblazing

--- docker/numa/dask_pyblazing.py
+++ docker/numa/dask_pyblazing.py
@@ -5,11 +5,22 @@ import time
 from dask.distributed import Client
 
 
-def run_query(index):
+def load_data(index):
+    print("load_data index:", index)
     column_names = ['n_nationkey', 'n_name', 'n_regionkey', 'n_comments']
     column_types = ['int32', 'int64', 'int32', 'int64']
     nation_gdf = cudf.read_csv("/blazingdb/data/nation.psv", delimiter='|', dtype=column_types, names=column_names)
     tables = {'nation': nation_gdf}
+    return tables
+
+
+column_names = ['n_nationkey', 'n_name', 'n_regionkey', 'n_comments']
+column_types = ['int32', 'int64', 'int32', 'int64']
+nation_gdf = cudf.read_csv("/blazingdb/data/nation.psv", delimiter='|', dtype=column_types, names=column_names)
+tables = {'nation': nation_gdf}
+
+def run_query(index):
+    print("run_query index:", index)
     sql = 'select n_nationkey, n_regionkey, n_nationkey + n_regionkey as addition from main.nation'
     result_gdf = pyblazing.run_query(sql, tables)
     tamanio = len(result_gdf.columns)
@@ -20,7 +31,7 @@ def run_query(index):
 
 
 client = Client('127.0.0.1:8786')
-for i in range(20):
-    print("i:", i)
-    total = client.submit(run_query, i)
-    print("result:", total.result())
+sum_demo = client.map(sum, [6])
+results = client.map(run_query, [8])
+total = client.submit(sum, results)
+print("result:", total.result())

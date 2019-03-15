1a3adad8e6e827b3bdc6ed8f608df9630b766902
mario21ic <Mario Inga Cahuana>
2019-03-15 15:02:36 -0500

    feat(dask) agregando volumen para compartir

--- docker/numa/dask_pyblazing2.py
+++ docker/numa/dask_pyblazing2.py
@@ -8,16 +8,17 @@ import time
 from dask.distributed import Client
 
 
-dir_path = '/tmp/tpch/'
+dir_path = '/blazingdb/data/tpch/'
 chunk_files = ['customer_0_0.parquet', 'customer_0_1.parquet']
 
-def run_query(index, tables):
+def run_query(index):
     print("run_query index:", index)
     print("load_data:", dir_path + chunk_files[index])
     customer_table = pyblazing.create_table(table_name='customer_parquet', type=SchemaFrom.ParquetFile, path= dir_path + chunk_files[index])
     nation_table = pyblazing.create_table(table_name='nation_parquet', type=SchemaFrom.ParquetFile, path= dir_path + '/nation_0_0.parquet')
-    tables = {'customer_parquet': customer_table.columns, 'nation_parquet': nation_table..columns}
+    tables = {'customer_parquet': customer_table.columns, 'nation_parquet': nation_table.columns}
 
+    #sql = 'select * from main.customer_parquet as c'
     sql = '''
         select avg(c.c_custkey), avg(c.c_nationkey), n.n_regionkey
         from main.customer_parquet as c
@@ -27,6 +28,7 @@ def run_query(index, tables):
     '''
 
     result_gdf = pyblazing.run_query(sql, tables)
+    print("result_gdf", result_gdf)
     tamanio = len(result_gdf.columns)
     print("tamanio:", tamanio)
     with open('/tmp/salida'+str(index)+'.txt', 'w') as file:
@@ -47,8 +49,8 @@ chunk_ids = [0, 1]
 workers_ips = ['172.18.0.23', '172.18.0.24']
 
 client = Client('127.0.0.1:8786')
-results = client.map(run_query, [chunk_ids], workers=workers_ips)
+results = client.map(run_query, chunk_ids, workers=workers_ips)
 
 total = client.submit(sum, results)
-print("x: %s - result: %s" % (chunk_ids, total.result()))
+print("result:", total.result())
 del client

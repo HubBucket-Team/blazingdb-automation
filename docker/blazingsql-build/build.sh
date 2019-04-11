fe205ddf8dbe0de44b14894ff0157cf103b938b5
Percy Camilo Triveño Aucahuasi <percy@blazingdb.com>
2019-04-11 15:47:26 -0500

    fix build after merge

--- docker/blazingsql-build/build.sh
+++ docker/blazingsql-build/build.sh
@@ -468,6 +468,9 @@ if [ $cudf_enable == true ]; then
         mkdir $cudf_branch_name
         cd $cudf_branch_name
         git clone git@github.com:BlazingDB/cudf.git
+        git checkout $cudf_branch
+        git pull
+        git submodule update --init --recursive
         
         echo "### CUDF - cmake ###"
         mkdir -p $libgdf_build_dir

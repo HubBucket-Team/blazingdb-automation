4258ac5cb911fda3cc7222500054efb220f6ae0c
mario21ic <Mario Inga Cahuana>
2019-05-06 17:19:21 -0500

    feat(colab) agregando script

---
+++ docker/colab/main.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+docker run --runtime=nvidia -ti nvidia/cuda:10.0-devel-ubuntu18.04 bash
+
+apt-get update
+apt-get install -y python3.7 python3-pip git vim
+apt-get install -y --no-install-recommends sudo bzip2 wget curl nano vim-tiny lsof htop net-tools
+apt-get install -y --no-install-recommends libicu55 libcurl3 libssl1.0.0 zlib1g libuuid1
+apt-get install -y supervisor openjdk-8-jre 
+
+wget https://github.com/Kitware/CMake/releases/download/v3.12.4/cmake-3.12.4-Linux-x86_64.sh
+bash cmake-3.12.4-Linux-x86_64.sh --skip-license --prefix=/usr
+
+CONTAINER_ID="a62a560bc8c6"
+docker cp ./cudf/cpp/install/lib/librmm.so a62a560bc8c6:/tmp/
+docker cp ./cudf/cpp/install/lib/libcudf.so a62a560bc8c6:/tmp/
+docker cp ./libhdfs3/libhdfs3.so a62a560bc8c6:/tmp/
+
+docker cp ./nvstrings/lib/libNVText.so a62a560bc8c6:/tmp/
+docker cp ./nvstrings/lib/libNVStrings.so a62a560bc8c6:/tmp/
+docker cp ./nvstrings/lib/libNVCategory.so a62a560bc8c6:/tmp/
+
+
+

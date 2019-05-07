1680fdb5a4668095f53befeb1cbb3b94a2e5aae3
mario21ic <Mario Inga Cahuana>
2019-05-07 12:03:44 -0500

    feat(colab) agregando install de cudf, protocol y pyblazing

--- docker/colab/main.sh
+++ docker/colab/main.sh
@@ -19,11 +19,17 @@ pip3 install pyarrow==0.12.1
 pip3 install pandas==0.24.2
 pip3 install numpy==1.16.2
 
+pip3 install cython
+
 # Previamente copiar blazingsql-files.tar.gz a /tmp/
 mkdir /tmp/blazing/
 cd /tmp/blazing/ && tar -xvf /tmp/blazingsql-files.tar.gz 
 
-# Install librmm 
+# libhdfs
+cp /tmp/blazing/blazingsql-files/libhdfs3/libhdfs3.so /usr/lib/
+
+# Install librmm
+cp /tmp/blazing/blazingsql-files/nvstrings-build/rmm/librmm.so /usr/lib/
 export RMM_HEADER=/tmp/blazing/blazingsql-files/cudf/cpp/thirdparty/rmm/include/rmm/rmm_api.h
 pip3 install /tmp/blazing/blazingsql-files/nvstrings-src/thirdparty/rmm/python/
 pip3 list
@@ -35,12 +41,31 @@ cp -rf /tmp/blazing/blazingsql-files/nvstrings/include/* /usr/include/
 pip3 install /tmp/blazing/blazingsql-files/nvstrings-src/python
 pip3 list
 
-# Install pyblazing
+# Install cudf
+cp -rf /tmp/blazing/blazingsql-files/cudf/cpp/install/lib/* /usr/lib/
+export CUDF_INCLUDE_DIR=/tmp/blazing/blazingsql-files/cudf/cpp/include/cudf/
+pip3 install /tmp/blazing/blazingsql-files/cudf/cpp/python
+CFLAGS="-I/tmp/blazing/blazingsql-files/cudf/cpp/install/include/ -I/tmp/blazing/blazingsql-files/cudf/thirdparty/dlpack/ -I/tmp/blazing/blazingsql-files/cudf/thirdparty/dlpack/include/dlpack -I/tmp/blazing/blazingsql-files/cudf/thirdparty/dlpack/include/" CXXFLAGS="-I/tmp/blazing/blazingsql-files/cudf/cpp/install/include/ -I/tmp/blazing/blazingsql-files/cudf/thirdparty/dlpack/ -I/tmp/blazing/blazingsql-files/cudf/thirdparty/dlpack/include/ -I/tmp/blazing/blazingsql-files/cudf/thirdparty/dlpack/include/dlpack" LD_FLASG="-L/usr/lib -lcudf" pip3 install /tmp/blazing/blazingsql-files/cudf/python/
+python3 -c "import cudf"
 
+# Install protocol
+pip3 install /tmp/blazing/blazingsql-files/blazingdb-protocol/python
+
+# Install pyblazing
+pip3 install /tmp/blazing/blazingsql-files/pyBlazing
 
 export NUMBAPRO_NVVM=/usr/local/cuda/nvvm/lib64/libnvvm.so
 export NUMBAPRO_LIBDEVICE=/usr/local/cuda/nvvm/libdevice/
 
+# Copiar binarios
+cp /tmp/blazing/blazingsql-files/BlazingCalcite.jar /usr/bin/
+cp /tmp/blazing/blazingsql-files/blazingdb_orchestator_service /usr/bin/
+cp /tmp/blazing/blazingsql-files/testing-libgdf /usr/bin/
+
+mkdir /blazingsql && chmod 777 /blazingsql
+
+# Copiar configs a /etc/supervisor/conf.d/ y ejecutar supervisor
+#supervisord -c /etc/supervisor/supervisord.conf
 
 #CONTAINER_ID="a62a560bc8c6"
 #docker cp ./cudf/cpp/install/lib/librmm.so a62a560bc8c6:/tmp/
@@ -51,10 +76,5 @@ export NUMBAPRO_LIBDEVICE=/usr/local/cuda/nvvm/libdevice/
 #docker cp ./nvstrings/lib/libNVStrings.so a62a560bc8c6:/tmp/
 #docker cp ./nvstrings/lib/libNVCategory.so a62a560bc8c6:/tmp/
 
-# Copiar binarios
-#cp BlazingCalcite.jar /usr/bin/
-#cp blazingdb_orchestator_service /usr/bin/
-#cp testing-libgdf /usr/bin/
 
-#supervisord -c /etc/supervisor/supervisord.conf
 

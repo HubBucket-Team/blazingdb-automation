da96623cdcff6f636de02898e10626a20d953117
kharoly <kharoly.cs@gmail.com>
2019-05-17 17:28:14 -0500

    change the configfile name

--- docker/blazingsql-testing/run_e2e.sh
+++ docker/blazingsql-testing/run_e2e.sh
@@ -4,6 +4,7 @@ echo "Waiting for apache drill on port 8047"
 while ! nc -z localhost 8047; do sleep 3; done
 
 workdir_home=$1
+module_test=$2
 
 cd $workdir_home/blazingdb-testing/BlazingSQLTest/
 echo "Activating cudf"
@@ -17,7 +18,8 @@ echo "PRINT configurationFile.json"
 
 #First execution
 echo "================================ First execution ================================"
-python -m EndToEndTests.allE2ETest  $workdir_home/configurationFileTrue.json
+#python -m EndToEndTests.parquetFromLocalTest  $workdir_home/configurationFile.json
+python -m EndToEndTests.$module_test  $workdir_home/configurationFileFalse.json
 
-echo "================================ First execution ================================"
-python -m EndToEndTests.allE2ETest  $workdir_home/configurationFileFalse.json
+echo "================================ Second execution ================================"
+python -m EndToEndTests.$module_test  $workdir_home/configurationFileTrue.json

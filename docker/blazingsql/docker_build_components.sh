530558c576dd7d7b16bce08a6864feaa5e117d94
Mario Inga <mario21ic@gmail.com>
2019-05-21 10:51:21 -0500

    fix(docker) adding build from docker-compose

--- docker/blazingsql/docker_build_components.sh
+++ docker/blazingsql/docker_build_components.sh
@@ -2,12 +2,12 @@
 
 echo "### Calcite ###"
 tar -xvf blazingsql-files.tar.gz blazingsql-files/BlazingCalcite.jar
-mv blazingsql-files/BlazingCalcite.jar calcite/
+cp -f blazingsql-files/BlazingCalcite.jar calcite/
 #docker build -t blazingdb/blazingsql:calcite calcite/
 
 echo "### Orchestrator ###"
 tar -xvf blazingsql-files.tar.gz blazingsql-files/blazingdb_orchestator_service
-mv blazingsql-files/blazingdb_orchestator_service orchestator/
+cp -f blazingsql-files/blazingdb_orchestator_service orchestator/
 #docker build -t blazingdb/blazingsql:orchestator orchestator/
 
 echo "### Ral ###"
@@ -16,13 +16,13 @@ tar -xvf blazingsql-files.tar.gz blazingsql-files/cudf/cpp/install/lib/librmm.so
 tar -xvf blazingsql-files.tar.gz blazingsql-files/nvstrings/lib/libNVCategory.so
 tar -xvf blazingsql-files.tar.gz blazingsql-files/nvstrings/lib/libNVStrings.so
 tar -xvf blazingsql-files.tar.gz blazingsql-files/testing-libgdf
-mv ./blazingsql-files/ ral/
+cp -rf ./blazingsql-files/ ral/
 #docker build -t blazingdb/blazingsql:ral ral/
 
 echo "### Pyblazing ###"
 cp -rf data/ pyblazing/
 cp -rf notebooks/ pyblazing/
-cp blazingsql-files.tar.gz pyblazing/
+cp -f blazingsql-files.tar.gz pyblazing/
 #docker build -t blazingdb/blazingsql:pyblazing pyblazing/
 
 echo "### Docker build ###"

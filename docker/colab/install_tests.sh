d9548661c5551c1e8accdaa79d599a3dee39735f
Mario Inga <mario21ic@gmail.com>
2019-05-24 16:44:27 -0500

    feat(cuda10) adding support to tests

---
+++ docker/colab/install_tests.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+apt-get install -y openjdk-8-jdk mysql-client git
+
+pip3 install pydrill
+pip3 install openpyxl
+pip3 install pymysql
+pip3 install gitpython
+
+wget -O /tmp/apache_drill.tar.gz -nv http://archive.apache.org/dist/drill/drill-1.12.0/apache-drill-1.12.0.tar.gz
+tar -xvzf /tmp/apache_drill.tar.gz -C /etc/
+echo "export DRILL_JAVA_OPTS='-Duser.timezone=UTC'" >> /etc/apache-drill-1.12.0/conf/drill-env.sh
+mkdir -p /etc/apache-drill-1.12.0/log/
+chmod 777 /etc/apache-drill-1.12.0/log/
---
+++ docker/colab/run_test.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export NUMBAPRO_NVVM=/usr/local/cuda/nvvm/lib64/libnvvm.so
+export NUMBAPRO_LIBDEVICE=/usr/local/cuda/nvvm/libdevice/
+
+cd /tmp/blazingdb/ && python3 -m EndToEndTests.allE2ETest configFileFalse.json

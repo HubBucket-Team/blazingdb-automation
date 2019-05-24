039e18863a2f5ebc864287e69a061549817e5de8
Mario Inga <mario21ic@gmail.com>
2019-05-24 17:03:16 -0500

    fix(cuda10) testing: adding support to tests

--- docker/colab/run_test.sh
+++ docker/colab/run_test.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# TODO: agregar parametros para module and dataset
 
 export NUMBAPRO_NVVM=/usr/local/cuda/nvvm/lib64/libnvvm.so
 export NUMBAPRO_LIBDEVICE=/usr/local/cuda/nvvm/libdevice/

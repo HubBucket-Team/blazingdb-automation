a54a84f695595d623d1224986a0d190ced6b9980
Mario Inga <mario21ic@gmail.com>
2019-06-07 18:19:02 -0500

    feat(k8s) ral_orchestrator: agregando ips como args

--- docker/blazingsql/ral_orchestrator/entrypoint.sh
+++ docker/blazingsql/ral_orchestrator/entrypoint.sh
@@ -18,11 +18,11 @@ echo "IP CALCITE: $IP_CALCITE"
 echo "IP LISTENING: $IP_LISTENING"
 
 echo "### Orchestrator ###"
+#/usr/bin/blazingdb_orchestator_service 172.21.1.3 8889 9000 172.21.1.2 8890 &
 echo "command: /usr/bin/blazingdb_orchestator_service $IP_LISTENING 8889 9000 $IP_CALCITE 8890 &"
-#/usr/bin/blazingdb_orchestator_service $IP_LISTENING 8889 9000 $IP_CALCITE 8890 &
-/usr/bin/blazingdb_orchestator_service 172.21.1.3 8889 9000 172.21.1.2 8890 &
+/usr/bin/blazingdb_orchestator_service $IP_LISTENING 8889 9000 $IP_CALCITE 8890 &
 
 echo "### Ral ###"
+#/usr/bin/testing-libgdf 1 172.21.1.3 8889 9000 127.0.0.1 8891 9001
 echo "command: /usr/bin/testing-libgdf 1 $IP_LISTENING 8889 9000 127.0.0.1 8891 9001"
-#/usr/bin/testing-libgdf 1 $IP_LISTENING 8889 9000 127.0.0.1 8891 9001
-/usr/bin/testing-libgdf 1 172.21.1.3 8889 9000 127.0.0.1 8891 9001
+/usr/bin/testing-libgdf 1 $IP_LISTENING 8889 9000 127.0.0.1 8891 9001

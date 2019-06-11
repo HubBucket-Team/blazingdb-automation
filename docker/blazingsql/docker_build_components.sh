7cb1c795764cb015aaa65c812c22de6e73805629
editaxz <edit44@outlook.com>
2019-06-11 15:02:15 -0500

    clean docker_build_componentes

--- docker/blazingsql/docker_build_components.sh
+++ docker/blazingsql/docker_build_components.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
-<<<<<<< HEAD
 component=$1
-=======
-echo "### Ucx ###"
-tar -xvf blazingsql-files.tar.gz blazingsql-files/ucx/
-cp -rf blazingsql-files/ucx orchestator/
-cp -rf blazingsql-files/ucx ral/
->>>>>>> feature/simple-distribution-tcp
 
 echo "### Calcite ###"
 tar -xvf blazingsql-files.tar.gz blazingsql-files/BlazingCalcite.jar
@@ -16,12 +9,8 @@ mv blazingsql-files/BlazingCalcite.jar calcite/
 
 echo "### Orchestrator ###"
 tar -xvf blazingsql-files.tar.gz blazingsql-files/blazingdb_orchestator_service
-<<<<<<< HEAD
 cp -f blazingsql-files/blazingdb_orchestator_service orchestator/
 cp -f blazingsql-files/blazingdb_orchestator_service ral_orchestrator/
-=======
-mv blazingsql-files/blazingdb_orchestator_service orchestator/
->>>>>>> feature/simple-distribution-tcp
 #docker build -t blazingdb/blazingsql:orchestator orchestator/
 
 
@@ -31,12 +20,8 @@ tar -xvf blazingsql-files.tar.gz blazingsql-files/cudf/cpp/install/lib/librmm.so
 tar -xvf blazingsql-files.tar.gz blazingsql-files/nvstrings/lib/libNVCategory.so
 tar -xvf blazingsql-files.tar.gz blazingsql-files/nvstrings/lib/libNVStrings.so
 tar -xvf blazingsql-files.tar.gz blazingsql-files/testing-libgdf
-<<<<<<< HEAD
 cp -rf ./blazingsql-files/ ral/
 cp -rf ./blazingsql-files/ ral_orchestrator
-=======
-mv ./blazingsql-files/ ral/
->>>>>>> feature/simple-distribution-tcp
 #docker build -t blazingdb/blazingsql:ral ral/
 
 echo "### Pyblazing ###"

9d13239982176a97ed9522a5645c56ad28eaa601
Mario Inga <mario21ic@gmail.com>
2019-06-17 17:52:26 -0500

    feat(docker) reemplazando conda por pip

--- docker/blazingsql/setup.sh
+++ docker/blazingsql/setup.sh
@@ -1,70 +1,95 @@
 #!/bin/bash
 
-working_directory=$PWD
+#cd /tmp/blazing/ && tar -xf /tmp/blazingsql-files.tar.gz
 blazingsql_files=/tmp/blazing/blazingsql-files
 
-cd /tmp/blazing/
-echo "Decompressing blazingsql-files.tar.gz ..."
-tar xf blazingsql-files.tar.gz
-echo "blazingsql-files.tar.gz was decompressed at /tmp/blazing/"
+echo "### libhdfs ###"
+cp -f $blazingsql_files/libhdfs3/libhdfs3.so /usr/lib/
+if [ $? != 0 ]; then
+  exit 1
+fi
 
-cd $blazingsql_files
-
-source activate cudf
-
-# Install libgdf
-cudf_dir=$blazingsql_files/cudf
-libgdf_dir=cpp
-
-cp $cudf_dir/$libgdf_dir/install/lib/libcudf.so /conda/envs/cudf/lib/
-cp $blazingsql_files/nvstrings-build/rmm/librmm.so /conda/envs/cudf/lib/
-
-# Install libgdf
-cp -r $cudf_dir/$libgdf_dir/install/* /conda/envs/cudf/
-cp -r $cudf_dir/$libgdf_dir/include/* /conda/envs/cudf/include/
-
-# Install libhdfs3
-cp -r $blazingsql_files/libhdfs3/* /usr/lib
-
-# Install rmm (from nvstrings)
-cp -f $blazingsql_files/nvstrings-build/rmm/*.so /conda/envs/cudf/lib/
-
-# Install nvstrings (custrings)
-cp -f $blazingsql_files/nvstrings-build/*.so /conda/envs/cudf/lib/
-cp -rf $blazingsql_files/nvstrings/include/* /conda/envs/cudf/include/
-
-echo "Installing custrings ..."
-# Clean old python/build folder
-rm -rf $blazingsql_files/nvstrings-src/python/build/
-working_directory_tmp=$PWD
-cd $blazingsql_files/nvstrings-src/python/
-python setup.py install
-cd $working_directory_tmp
-echo "custrings DONE"
-
-# Install libgdf_cffi
-#sed -i 's/..\/..\//\/tmp\/blazing\/blazingsql-files\/cudf\/cpp\//g' $cudf_dir/$libgdf_dir/python/libgdf_cffi/libgdf_build.py
+echo "### librmm ###"
+cp -f $blazingsql_files/nvstrings-build/rmm/librmm.so /usr/lib/
+if [ $? != 0 ]; then
+  exit 1
+fi
+#export RMM_HEADER=$blazingsql_files/cudf/cpp/thirdparty/rmm/include/rmm/rmm_api.h
+#pip3 install $blazingsql_files/nvstrings-src/thirdparty/rmm/python/
 sed -i 's/..\/..\//\/tmp\/blazing\/blazingsql-files\/cudf\/cpp\//g' $blazingsql_files/nvstrings-src/thirdparty/rmm/python/librmm_cffi/librmm_build.py
+RMM_HEADER=$blazingsql_files/cudf/cpp/thirdparty/rmm/include/rmm/rmm_api.h $PIP install $blazingsql_files/nvstrings-src/thirdparty/rmm/python/
+if [ $? != 0 ]; then
+  exit 1
+fi
+#pip3 list
 
-#pip install $cudf_dir/$libgdf_dir/python
-RMM_HEADER=/tmp/blazing/blazingsql-files/cudf/cpp/thirdparty/rmm/include/rmm/rmm_api.h pip install $blazingsql_files/nvstrings-src/thirdparty/rmm/python/
+echo "### custrings ###"
+cp $blazingsql_files/nvstrings-build/libNV* /usr/lib/
+if [ $? != 0 ]; then
+  exit 1
+fi
+cp -rf $blazingsql_files/nvstrings/include/* /usr/include/
+if [ $? != 0 ]; then
+  exit 1
+fi
+#export NVSTRINGS_INCLUDE=$blazingsql_files/nvstrings/include/
+rm -rf $blazingsql_files/nvstrings-src/python/build/
+$PIP install $blazingsql_files/nvstrings-src/python
+if [ $? != 0 ]; then
+  exit 1
+fi
+#pip3 list
 
-# Install cudf
-CFLAGS="-I/conda/envs/cudf/include -I$cudf_dir/thirdparty/dlpack/include/dlpack -I$cudf_dir/thirdparty/dlpack/include/" CXXFLAGS="-I/conda/envs/cudf/include -I$cudf_dir/thirdparty/dlpack/include/dlpack -I$cudf_dir/thirdparty/dlpack/include/" pip install $cudf_dir/python
+echo "### cudf ###"
+cp -rf $blazingsql_files/cudf/cpp/install/lib/* /usr/lib/
+cp -rf $blazingsql_files/cudf/thirdparty/dlpack/include/dlpack/* /usr/lib/
+if [ $? != 0 ]; then
+  exit 1
+fi
+cp -rf $blazingsql_files/cudf/cpp/include/* /usr/include/
+if [ $? != 0 ]; then
+  exit 1
+fi
+export CUDF_INCLUDE_DIR=$blazingsql_files/cudf/cpp/include/cudf/
+$PIP install $blazingsql_files/cudf/cpp/python/
+if [ $? != 0 ]; then
+  exit 1
+fi
 
-# Install blazingsql
-blazingdb_ral_artifact_name=testing-libgdf
-blazingdb_orchestrator_artifact_name=blazingdb_orchestator_service
-blazingdb_calcite_artifact_name=BlazingCalcite.jar
+sed -i 's/..\/cpp\/include\//\/tmp\/blazing\/blazingsql-files\/cudf\/cpp\/include\//g' $blazingsql_files/cudf/python/setup.py
+sed -i 's/..\/cpp\/thirdparty\/dlpack\/include\/dlpack\//\/tmp\/blazing\/blazingsql-files\/cudf\/thirdparty\/dlpack\/include\/dlpack\//g' $blazingsql_files/cudf/python/setup.py
+CFLAGS="-I/tmp/blazing/blazingsql-files/cudf/cpp/install/include/ -I/tmp/blazing/blazingsql-files/cudf/thirdparty/dlpack/include/dlpack/" CXXFLAGS="-I/tmp/blazing/blazingsql-files/cudf/cpp/install/include/ -I/tmp/blazing/blazingsql-files/cudf/thirdparty/dlpack/include/dlpack/" LD_FLASG="-L/usr/lib -lcudf" $PIP install $blazingsql_files/cudf/python/
+if [ $? != 0 ]; then
+  exit 1
+fi
 
-cp $blazingsql_files/$blazingdb_ral_artifact_name /home/jupyter
-cp $blazingsql_files/$blazingdb_orchestrator_artifact_name /home/jupyter
-cp $blazingsql_files/$blazingdb_calcite_artifact_name /home/jupyter
+echo "### test cudf ###"
+export NUMBAPRO_NVVM=/usr/local/cuda/nvvm/lib64/libnvvm.so
+export NUMBAPRO_LIBDEVICE=/usr/local/cuda/nvvm/libdevice/
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda/lib64/:/usr/lib/x86_64-linux-gnu/
+# $PYTHON -c "import cudf"
+# if [ $? != 0 ]; then
+#   exit 1
+# fi
 
-# Install blazingdb-protocol/python
-pip install $blazingsql_files/blazingdb-protocol/python/
+echo "### protocol ###"
+$PIP install $blazingsql_files/blazingdb-protocol/python
+if [ $? != 0 ]; then
+  exit 1
+fi
 
-# Install pyblazing
-pip install $blazingsql_files/pyBlazing
+echo "### pyblazing ###"
+$PIP install $blazingsql_files/pyBlazing
+if [ $? != 0 ]; then
+  exit 1
+fi
+echo "### test pyblazing ###"
+# $PYTHON -c "import pyblazing"
+# if [ $? != 0 ]; then
+#   exit 1
+# fi
 
-cd $working_directory
+echo "### binaries ###"
+cp -f $blazingsql_files/BlazingCalcite.jar /usr/bin/
+cp -f $blazingsql_files/blazingdb_orchestator_service /usr/bin/
+cp -f $blazingsql_files/testing-libgdf /usr/bin/

88092e4e8e3471cd84d9a6a3a0a1d6eab6812ee7
Mario Inga <mario21ic@gmail.com>
2019-06-20 02:23:02 -0500

    fix(colab) actualizando demo2.py

--- docker/colab/demo2.py
+++ docker/colab/demo2.py
@@ -1,6 +1,6 @@
 import cudf
-from blazingsql import BlazingContext
 import pandas as pd
+from blazingsql import BlazingContext
 
 bc = BlazingContext()
 
@@ -8,14 +8,11 @@ column_names = ['n_nationkey', 'n_name', 'n_regionkey', 'n_comments']
 column_types = {'n_nationkey': 'int32', 'n_regionkey': 'int64'}
 nation_df = pd.read_csv("/blazingsql/data/nation.psv", delimiter='|',
                         dtype=column_types, names=column_names)
-nation_df = nation_df[['n_nationkey', 'n_regionkey']]
-
 print(nation_df)
 
-bc.create_table('nation', nation_gdf)
+bc.create_table('nation', nation_df)
 query = 'select n_nationkey, n_regionkey, n_nationkey + n_regionkey as addition from main.nation'
 result_gdf = bc.sql(query).get()
 
 print(query)
 print(result_gdf)
-

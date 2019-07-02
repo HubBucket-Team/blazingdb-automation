7b777d1a78766848bc703e31d559584f2c458125
edith <edit44@outlook.com>
2019-07-02 12:18:16 -0500

    change to ubuntu16.04

--- docker/cuda10_run.sh
+++ docker/cuda10_run.sh
@@ -5,4 +5,4 @@ CONTAINER_NAME=$1
 
 docker rm -f $CONTAINER_NAME
 
-docker run --runtime=nvidia --name $CONTAINER_NAME -d -ti nvidia/cuda:10.0-devel-ubuntu18.04 bash
+docker run --runtime=nvidia --name $CONTAINER_NAME -d -ti nvidia/cuda:10.0-devel-ubuntu16.04 bash

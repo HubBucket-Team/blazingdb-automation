25c143e2e5f51d2adadecf1c61e1753f2ac97560
editaxz <edit44@outlook.com>
2019-07-04 00:19:47 -0500

    adding logs into s3

--- docker/blazingsql-testing/endtoend_test_logs.sh
+++ docker/blazingsql-testing/endtoend_test_logs.sh
@@ -7,12 +7,13 @@ logs_path=s3://blazingsql-colab/blazingsql-logs/endtoend_test_blazingsql
 # Copy from docker container to tmp directory
 sudo mkdir /tmp/$build_number
 
-echo "Copying logs into localmachine"
+echo "Copying logs into local machine jenkins"
 sudo docker cp bzsqlcontainer:/var/log/supervisor  /tmp/$build_number
 
 #  Uploading logs to s3 public bucket
+echo "Copying logs to s3"
 aws s3 cp  /tmp/$build_number/ $logs_path/$build_number --recursive
-aws s3 cp /tmp/supervisor/* $logs_path
+#aws s3 cp /tmp/supervisor/* $logs_path
 
 # Print logs
 echo "The generated logs are:"

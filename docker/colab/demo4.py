c48f438a0be9712703e2e264630d580ef8a19aec
Mario Inga <mario21ic@gmail.com>
2019-07-04 18:36:49 -0500

    feat(cuda10) jenkins: agregando demo4.py

--- docker/colab/demo4.py
+++ docker/colab/demo4.py
@@ -1,20 +1,24 @@
 import sys, os
-os.environ["NUMBAPRO_NVVM"] = "/usr/local/cuda/nvvm/lib64/"
+os.environ["NUMBAPRO_NVVM"] = "/usr/local/cuda/nvvm/lib64/libnvvm.so"
 os.environ["NUMBAPRO_LIBDEVICE"] = "/usr/local/cuda/nvvm/libdevice/"
 
+from blazingsql import BlazingContext
 import cudf
-import pyblazing
 
-column_names = ['n_nationkey', 'n_name', 'n_regionkey', 'n_comments']
-column_types = ['int32', 'str', 'int32', 'str']
-nation_gdf = cudf.read_csv("/blazingsql/data/nation.psv", delimiter='|',
-                           dtype=column_types, names=column_names)
+bc = BlazingContext()
 
-print(nation_gdf)
+# Read cvs
+gdf = cudf.read_csv('/blazingsql/data/Music.csv')
 
-tables = {'nation': nation_gdf}
-sql = 'select n_name from main.nation'
-result_gdf = pyblazing.run_query(sql, tables)
+# Create table
+bc.create_table('music', gdf)
 
-print(sql)
+# Query
+result = bc.sql('SELECT * FROM main.music', ['music']).get()
+
+# Get GDF
+result_gdf = result.columns
+
+# Print GDF
 print(result_gdf)
+

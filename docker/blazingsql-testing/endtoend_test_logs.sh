d7d05bbbb7e772832e73b2f6c09d7cf76d16811f
edith <edit44@outlook.com>
2019-07-04 12:26:50 -0500

    update script to update logs

--- docker/blazingsql-testing/endtoend_test_logs.sh
+++ docker/blazingsql-testing/endtoend_test_logs.sh
@@ -9,17 +9,20 @@ sudo mkdir /tmp/$build_number
 
 echo "Copying logs into local machine jenkins"
 sudo docker cp bzsqlcontainer:/var/log/supervisor  /tmp/$build_number
-sudo chmod  777  -R /tmp/$build_number
+sudo chmod  644  -R /tmp/$build_number
 #  Uploading logs to s3 public bucket
 echo "Copying logs to s3"
 aws s3 cp  /tmp/$build_number/ $logs_path/$build_number --recursive
-#aws s3 cp /tmp/supervisor/* $logs_path
 
 # Print logs
-echo "=========== Listing Logs ======================"
-aws s3 ls $logs_path/$build_number/supervisor/
+#echo "=========== Listing Logs ======================"
+#aws s3 ls $logs_path/$build_number/supervisor/
 
-echo "check  the logs in your local machine"
-echo "wget https://blazingsql-colab.s3.amazonaws.com/blazingsql-logs/endtoend_test_blazingsql/$build_number/supervisor/"
+# Pruebas
 
-#bzsqlcontainer
+echo "================ Execute this command to check the logs from S3 ============================"
+
+for f in $(aws s3 ls $logs_path/$build_number/supervisor/ | awk '{print $NF}'); 
+do       
+    echo  "wget https://blazingsql-colab.s3.amazonaws.com/blazingsql-logs/endtoend_test_blazingsql/$build_number/supervisor/$f";
+done

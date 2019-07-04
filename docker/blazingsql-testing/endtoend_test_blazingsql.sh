64970318591a786c05d18d1cc0d73e58d058a73b
editaxz <edit44@outlook.com>
2019-07-03 21:52:48 -0500

    add sudo

--- docker/blazingsql-testing/endtoend_test_blazingsql.sh
+++ docker/blazingsql-testing/endtoend_test_blazingsql.sh
@@ -5,7 +5,7 @@ build_number=$1
 logs_path=s3://blazingsql-colab/blazingsql-logs/endtoend_test_blazingsql
 
 # Copy from docker container to tmp directory
-mkdir /tmp/$build_number
+sudo mkdir /tmp/$build_number
 docker cp myjupyter:/var/log/supervisor  /tmp/$build_number
 
 #  Uploading logs to s3 public bucket

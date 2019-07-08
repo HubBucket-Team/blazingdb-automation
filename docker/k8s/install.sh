4392ef00c7ebd4e92889c957ee5febcedf6989da
Mario Inga <mario21ic@gmail.com>
2019-07-08 12:04:30 -0500

    feat(k8s) actualizando install.sh para soportar gpu type

--- docker/k8s/install.sh
+++ docker/k8s/install.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
-# Syntax: ./create_cluster.sh project_name cluster_name zone number_nodes number_gpus_per_node directory_yaml/
-# Example: ./create_cluster.sh myproject-bigdata cluster-2-2gpu us-west1-a 2 2 tcp/
+# Syntax: ./create_cluster.sh project_name cluster_name zone number_nodes number_gpus_per_node gpu_type directory_yaml/
+# Example: ./create_cluster.sh myproject-bigdata cluster-2-2gpu us-west1-a 2 2 v100|p100|k80|t4 tcp/
 
 project=$1
 name=$2
 zone=$3
 num_nodes=$4
 num_gpus=$5
-recipe=$6
+gpu_type="t4"
+if [ ! -z $6 ];
+then
+  gpu_type=$6
+fi
+recipe=$7
 region="${zone::-2}"
+#echo "gpu_type: $gpu_type"
 
 
 echo "### creating cluster ###"
-gcloud beta container --project "$project" clusters create "$name" --zone "$zone" --username "admin" --cluster-version "1.12.8-gke.10" --machine-type "n1-standard-4" --accelerator "type=nvidia-tesla-t4,count=$num_gpus" --image-type "COS" --disk-type "pd-standard" --disk-size "50" --scopes "https://www.googleapis.com/auth/devstorage.read_only","https://www.googleapis.com/auth/logging.write","https://www.googleapis.com/auth/monitoring","https://www.googleapis.com/auth/servicecontrol","https://www.googleapis.com/auth/service.management.readonly","https://www.googleapis.com/auth/trace.append" --num-nodes "$num_nodes" --enable-cloud-logging --enable-cloud-monitoring --no-enable-ip-alias --network "projects/$project/global/networks/default" --subnetwork "projects/$project/regions/$region/subnetworks/default" --addons HorizontalPodAutoscaling,HttpLoadBalancing --enable-autoupgrade --enable-autorepair
+gcloud beta container --project "$project" clusters create "$name" --zone "$zone" --username "admin" --cluster-version "1.12.8-gke.10" --machine-type "n1-standard-4" --accelerator "type=nvidia-tesla-$gpu_type,count=$num_gpus" --image-type "COS" --disk-type "pd-standard" --disk-size "50" --scopes "https://www.googleapis.com/auth/devstorage.read_only","https://www.googleapis.com/auth/logging.write","https://www.googleapis.com/auth/monitoring","https://www.googleapis.com/auth/servicecontrol","https://www.googleapis.com/auth/service.management.readonly","https://www.googleapis.com/auth/trace.append" --num-nodes "$num_nodes" --enable-cloud-logging --enable-cloud-monitoring --no-enable-ip-alias --network "projects/$project/global/networks/default" --subnetwork "projects/$project/regions/$region/subnetworks/default" --addons HorizontalPodAutoscaling,HttpLoadBalancing --enable-autoupgrade --enable-autorepair
 if [ $? != 0 ]; then
   exit 1
 fi

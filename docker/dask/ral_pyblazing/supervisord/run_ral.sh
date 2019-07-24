5ad5f0ef161f6bbff475afea99569c674758a5f2
Mario Inga <mario21ic@gmail.com>
2019-07-24 14:28:13 -0500

    feat(dask) agregando instalacion de rmm, cudf y nvstrings mediante conda

--- docker/dask/ral_pyblazing/supervisord/run_ral.sh
+++ docker/dask/ral_pyblazing/supervisord/run_ral.sh
@@ -4,4 +4,4 @@ export IP=$(ifconfig eth0 | grep "inet addr" | cut -d ':' -f 2 | cut -d ' ' -f 1
 
 export LD_LIBRARY_PATH="/usr/local/nvidia/lib64:/conda/envs/cudf/lib/:/usr/local/cuda-9.2/targets/x86_64-linux/lib/stubs/"
 
-/home/jupyter/testing-libgdf 1 blazingdb-dask-scheduler-svc 9000 $IP 9000 8891
+/home/jupyter/testing-libgdf 1 0 blazingdb-dask-scheduler-svc 9000 $IP 9000 8891

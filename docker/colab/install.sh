ec9555e87911f518d80b6fc3d0b4259abe4cf040
Mario Inga <mario21ic@gmail.com>
2019-08-07 11:04:23 -0500

    feat(colab) updating script to install with conda

--- docker/colab/install.sh
+++ docker/colab/install.sh
@@ -1,13 +1,51 @@
 #!/bin/bash
-# Usage: path_tar verbose sudo path_usr
-# Usage: /tmp/blazingsql-files.tar.gz true true $PYENV_VIRTUAL_ENV/
+# Usage: path_tar verbose sudo path_usr non_strict_mode
+# Usage: /tmp/blazingsql-files.tar.gz true true $PYENV_VIRTUAL_ENV/|/usr true
 
-BUCKET="blazingsql-colab/latest"
+GPU_TYPE=$(nvidia-smi --query-gpu=gpu_name --format=csv|awk 'FNR==2 {print $1}')
+echo "GPU_TYPE: "$GPU_TYPE
+
+STRICT_MODE=0
+if [ ! -z $5 ]; then
+  STRICT_MODE=1
+fi
+
+echo "Strict mode: "$STRICT_MODE
+if [ $STRICT_MODE == 1 ] && [ "$GPU_TYPE" != "Tesla T4" ]; then
+  echo "GPU model must be Tesla T4"
+  exit 1
+fi
+
+VERBOSE="/dev/null"
+if [ ! -z $2 ]; then
+  VERBOSE="/dev/stdout"
+fi
+
+SUDO=""
+if [ ! -z $3 ]; then
+  SUDO="sudo"
+fi
+
+echo "### update ###"
+$SUDO apt-get update -qq > $VERBOSE
+
+echo "### wget ###"
+$SUDO apt-get install -y wget > $VERBOSE
+if [ $? != 0 ]; then
+    exit 1
+fi
+
+echo "### dependencies ###"
+$SUDO apt-get install -y --no-install-recommends libprotobuf-dev libgsasl7 libgsasl7-dev libxml2-dev libicu-dev libcurl3 libssl1.0.0 zlib1g libuuid1 > $VERBOSE
+if [ $? != 0 ]; then
+    exit 1
+fi
+
+BUCKET="blazingsql-colab"
 BUCKET_DEMO=$BUCKET"/demo"
 BUCKET_DATA=$BUCKET_DEMO"/data"
 
-if [ -z $1 ];
-then
+if [ -z $1 ]; then
   wget -O /tmp/blazingsql-files.tar.gz -q https://s3.amazonaws.com/$BUCKET/blazingsql-files.tar.gz
   if [ $? != 0 ]; then
     exit 1
@@ -19,215 +57,66 @@ if [ $? != 0 ]; then
   exit 1
 fi
 
-PYTHON="python3"
-#PIP="$PYTHON -m pip"
-PIP="pip3"
-
-VERBOSE="/dev/null"
-if [ ! -z $2 ];
-then
-  VERBOSE="/dev/stdout"
-fi
-
-SUDO=""
-if [ ! -z $3 ];
-then
-  SUDO="sudo"
-fi
+PYTHON="python3.7"
+PIP="python3.7 -m pip"
 
 PATH_USR="/usr/"
-if [ ! -z $4 ];
-then
+if [ ! -z $4 ]; then
   PATH_USR=$4
 fi
 
+source /etc/lsb-release
+#echo "DISTRIB_ID: "$DISTRIB_ID
+#echo "DISTRIB_RELEASE: "$DISTRIB_RELEASE
+echo "DISTRIB_DESCRIPTION: "$DISTRIB_DESCRIPTION
 echo "PYTHON: "$PYTHON
-echo "VERSION: "$($PYTHON --version)
+#echo "VERSION: "$($PYTHON --version)
 echo "PIP: "$PIP
 echo "VERBOSE: "$VERBOSE
 echo "SUDO: "$SUDO
 echo "PATH_USR: "$PATH_USR
 
-# source /etc/lsb-release
-# if [ $DISTRIB_RELEASE == "16.04" ];
-# then
-#   echo "Installing python 3.7"
-#   $SUDO apt-get update -qq > $VERBOSE
-#   if [ $? != 0 ]; then
-#     exit 1
-#   fi
-
-#   $SUDO apt-get install -y software-properties-common > $VERBOSE
-#   $SUDO add-apt-repository -y ppa:deadsnakes/ppa
-#   if [ $? != 0 ]; then
-#     exit 1
-#   fi
-# fi
-
-echo "### update ###"
-$SUDO apt-get update -qq > $VERBOSE
-
-echo "### dependencies ###"
-#ln -s /usr/bin/python3 /usr/bin/python
-#ln -s /usr/bin/pip3 /usr/bin/pip
-#apt-get install -y -qq git vim > $VERBOSE
-$SUDO apt-get install -y -qq --fix-missing --no-install-recommends \
-  $PYTHON $PYTHON-pip $PYTHON-dev \
-  libffi-dev libgsasl7 libgsasl7-dev \
-  bzip2 wget curl lsof \
-  libssl1.0.0 zlib1g libuuid1 \
-  supervisor openjdk-8-jre > $VERBOSE
-if [ $? != 0 ]; then
-    exit 1
-fi
-$SUDO apt-get install -y -qq libcurl3
+echo "### java ###"
+echo "CMD: $SUDO apt-get install -y openjdk-8-jre"
+$SUDO apt-get install -y openjdk-8-jre > $VERBOSE
 if [ $? != 0 ]; then
     exit 1
 fi
 
-echo "### cmake ###"
-if ! [ -x "$(command -v cmake)" ]; then
-  echo "cmake not installed"
-  wget -q https://github.com/Kitware/CMake/releases/download/v3.14.3/cmake-3.14.3-Linux-x86_64.sh
-  bash cmake-3.14.3-Linux-x86_64.sh --skip-license --prefix=/usr
+echo "### Conda ###"
+if ! [ -x "$(command -v conda)" ]; then
+  echo "conda is not installed"
+  wget -q -O /tmp/miniconda.sh -nv https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
+  bash /tmp/miniconda.sh -b -f -p /usr/local/
   if [ $? != 0 ]; then
     exit 1
   fi
-  rm -f cmake-3.14.3-Linux-x86_64.sh
+  rm -f /tmp/miniconda.sh
 fi
-echo "CMAKE version: "$(cmake --version)
+echo "conda version: "$(conda --version)
 if [ $? != 0 ]; then
   exit 1
 fi
 
-echo "### pip dependencies ###"
-$PIP install --upgrade --force-reinstall setuptools
-#$PIP install --upgrade pip
-
-$PIP install wheel==0.32.1 > $VERBOSE
-if [ $? != 0 ]; then
-  exit 1
-fi
-$PIP install cmake_setuptools > $VERBOSE
-if [ $? != 0 ]; then
-  exit 1
-fi
-$PIP install numba==0.43.0 > $VERBOSE
-if [ $? != 0 ]; then
-  exit 1
-fi
-$PIP install numpy==1.16.2 > $VERBOSE
-if [ $? != 0 ]; then
-  exit 1
-fi
-$PIP install pandas==0.24.2 > $VERBOSE
-if [ $? != 0 ]; then
-  exit 1
-fi
-$PIP install pyarrow==0.12.1 > $VERBOSE
+echo "### conda dependencies ###"
+conda install -y -c numba -c conda-forge -c defaults dask xgboost numba=0.43.0 pandas=0.24.2 pyarrow=0.12.1 cmake=3.14
 if [ $? != 0 ]; then
   exit 1
 fi
 
-$PIP install flatbuffers > $VERBOSE
-if [ $? != 0 ]; then
-  exit 1
-fi
+#echo "### pip dependencies ###"
+#$PIP install sklearn 
+#if [ $? != 0 ]; then
+#  exit 1
+#fi
 
-$PIP install cython > $VERBOSE
-if [ $? != 0 ]; then
-  exit 1
-fi
-$PIP install xgboost > $VERBOSE
-if [ $? != 0 ]; then
-  exit 1
-fi
-$PIP install sklearn > $VERBOSE
-if [ $? != 0 ]; then
-  exit 1
-fi
-
-
-blazingsql_files=/tmp/blazing/blazingsql-files
-
-PYTHON_INCLUDE_DIR=$($PYTHON -c "from distutils.sysconfig import get_python_inc; print(get_python_inc())")
-PYTHON_LIBRARY=$($PYTHON -c "import distutils.sysconfig as sysconfig; import os; print(os.path.join(sysconfig.get_config_var('LIBDIR'), sysconfig.get_config_var('LDLIBRARY')))")
-CMAKE_COMMON_VARIABLES="-DPYTHON_INCLUDE_DIR=$PYTHON_INCLUDE_DIR -DPYTHON_LIBRARY=$PYTHON_LIBRARY"
-
-echo "PYTHON_INCLUDE_DIR: $PYTHON_INCLUDE_DIR"
-echo "PYTHON_LIBRARY: $PYTHON_LIBRARY"
-echo "CMAKE_COMMON_VARIABLES: $CMAKE_COMMON_VARIABLES"
-
-echo "### libhdfs ###"
-cp -f $blazingsql_files/libhdfs3/libhdfs3.so $PATH_USR/lib/
+echo "### conda nightly ###"
+conda install -y -c rapidsai-nightly/label/cuda10.0 rmm=0.9.0a1 nvstrings=0.9.0a cudf=0.9 dask-cudf=0.9.0a
 if [ $? != 0 ]; then
   exit 1
 fi
 
-echo "### librmm ###"
-cp -f $blazingsql_files/nvstrings-build/rmm/librmm.so $PATH_USR/lib/
-if [ $? != 0 ]; then
-  exit 1
-fi
-#export RMM_HEADER=$blazingsql_files/cudf/cpp/thirdparty/rmm/include/rmm/rmm_api.h
-#pip3 install $blazingsql_files/nvstrings-src/thirdparty/rmm/python/
-sed -i 's/..\/..\//\/tmp\/blazing\/blazingsql-files\/cudf\/cpp\//g' $blazingsql_files/nvstrings-src/thirdparty/rmm/python/librmm_cffi/librmm_build.py
-RMM_HEADER=$blazingsql_files/cudf/cpp/thirdparty/rmm/include/rmm/rmm_api.h $PIP install $blazingsql_files/nvstrings-src/thirdparty/rmm/python/
-if [ $? != 0 ]; then
-  exit 1
-fi
-#pip3 list
-
-echo "### custrings ###"
-cp $blazingsql_files/nvstrings-build/libNV* $PATH_USR/lib/
-if [ $? != 0 ]; then
-  exit 1
-fi
-cp -rf $blazingsql_files/nvstrings/include/* $PATH_USR/include/
-if [ $? != 0 ]; then
-  exit 1
-fi
-#export NVSTRINGS_INCLUDE=$blazingsql_files/nvstrings/include/
-rm -rf $blazingsql_files/nvstrings-src/python/build/
-#$PIP install $blazingsql_files/nvstrings-src/python
-cd $blazingsql_files/nvstrings-src/python && CUDACXX=/usr/local/cuda/bin/nvcc NVSTRINGS_ROOT=$PATH_USR $PYTHON setup.py install
-if [ $? != 0 ]; then
-  exit 1
-fi
-echo "### test custrings ###"
-$PYTHON -c "import nvstrings, nvcategory"
-#pip3 list
-
-echo "### cudf ###"
-cp -rf $blazingsql_files/cudf/cpp/install/lib/* $PATH_USR/lib/
-cp -rf $blazingsql_files/cudf/thirdparty/dlpack/include/dlpack/* $PATH_USR/lib/
-if [ $? != 0 ]; then
-  exit 1
-fi
-cp -rf $blazingsql_files/cudf/cpp/include/* $PATH_USR/include/
-if [ $? != 0 ]; then
-  exit 1
-fi
-export CUDF_INCLUDE_DIR=$blazingsql_files/cudf/cpp/include/cudf/
-#$PIP install $blazingsql_files/cudf/cpp/python/
-if [ $? != 0 ]; then
-  exit 1
-fi
-
-sed -i 's/..\/cpp\/include\//\/tmp\/blazing\/blazingsql-files\/cudf\/cpp\/include\//g' $blazingsql_files/cudf/python/setup.py
-sed -i 's/..\/cpp\/thirdparty\/dlpack\/include\/dlpack\//\/tmp\/blazing\/blazingsql-files\/cudf\/thirdparty\/dlpack\/include\/dlpack\//g' $blazingsql_files/cudf/python/setup.py
-CFLAGS="-I/tmp/blazing/blazingsql-files/cudf/cpp/install/include/ -I/tmp/blazing/blazingsql-files/cudf/thirdparty/dlpack/include/dlpack/" CXXFLAGS="-I/tmp/blazing/blazingsql-files/cudf/cpp/install/include/ -I/tmp/blazing/blazingsql-files/cudf/thirdparty/dlpack/include/dlpack/" LD_FLASG="-L$PATH_USR/lib -lcudf" $PIP install $blazingsql_files/cudf/python/
-if [ $? != 0 ]; then
-  exit 1
-fi
-
-echo "### test cudf ###"
-export NUMBAPRO_NVVM=/usr/local/cuda/nvvm/lib64/libnvvm.so
-export NUMBAPRO_LIBDEVICE=/usr/local/cuda/nvvm/libdevice/
-$PYTHON -c "import cudf"
-if [ $? != 0 ]; then
-  exit 1
-fi
+blazingsql_files=/tmp/blazing/blazingsql-files
 
 echo "### protocol ###"
 $PIP install $blazingsql_files/blazingdb-protocol/python
@@ -256,6 +145,7 @@ $SUDO mkdir -p /blazingsql/data/ && chmod 777 -R /blazingsql
 echo "### downloading demo files ###"
 wget -q -O /blazingsql/data/nation.psv https://s3.amazonaws.com/$BUCKET_DATA/nation.psv
 wget -q -O /blazingsql/data/gpu.arrow https://s3.amazonaws.com/$BUCKET_DATA/gpu.arrow
+wget -q -O /blazingsql/data/Music.csv https://s3.amazonaws.com/$BUCKET_DATA/Music.csv
 wget -q -O /blazingsql/data/cancer_data_00.csv https://s3.amazonaws.com/$BUCKET_DATA/cancer_data_00.csv
 wget -q -O /blazingsql/data/cancer_data_01.parquet https://s3.amazonaws.com/$BUCKET_DATA/cancer_data_01.parquet
 wget -q -O /blazingsql/data/cancer_data_02.csv .csv https://s3.amazonaws.com/$BUCKET_DATA/cancer_data_02.csv
@@ -268,6 +158,7 @@ wget -q -O /blazingsql/demo5.py https://s3.amazonaws.com/$BUCKET_DEMO/demo5.py
 
 
 echo "### supervisor ###"
+$SUDO apt-get install -y supervisor > $VERBOSE
 wget -q -O /etc/supervisor/conf.d/blazing-calcite.conf https://s3.amazonaws.com/blazingsql-colab/supervisor/blazing-calcite.conf
 wget -q -O /etc/supervisor/conf.d/blazing-orchestrator.conf https://s3.amazonaws.com/blazingsql-colab/supervisor/blazing-orchestrator.conf
 wget -q -O /etc/supervisor/conf.d/blazing-ral.conf https://s3.amazonaws.com/blazingsql-colab/supervisor/blazing-ral.conf
@@ -277,7 +168,7 @@ wget -q -O /usr/bin/blazingsql https://s3.amazonaws.com/blazingsql-colab/blazing
 blazingsql status
 
 # Clean
-#apt-get clean
+apt-get clean
 rm -rf /tmp/blazing*
 
 echo "### BlazingSQL installation finished ###"

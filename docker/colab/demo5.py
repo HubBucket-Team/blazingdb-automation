97aac58ea7221db90c7f93dadb9a7550621254e4
Mario Inga <mario21ic@gmail.com>
2019-08-07 13:17:02 -0500

    feat(colab) updating demo5.py

--- docker/colab/demo5.py
+++ docker/colab/demo5.py
@@ -5,11 +5,11 @@ bc = BlazingContext()
 # Csv files
 column_names = ['diagnosis_result', 'radius', 'texture', 'perimeter']
 column_types = ['float32', 'float32', 'float32', 'float32']
-bc.create_table('data_00', '/blazingdb/data/cancer_data_00.csv', delimiter=',', dtype=column_types, names=column_names)
+bc.create_table('data_00', '/blazingsql/data/cancer_data_00.csv', delimiter=',', dtype=column_types, names=column_names)
 
 # Parquet
 column_names = ['perimeter', 'area', 'smoothness', 'compactness']
-bc.create_table('data_01_parquet', '/blazingdb/data/cancer_data_01.parquet')
+bc.create_table('data_01_parquet', '/blazingsql/data/cancer_data_01.parquet')
 query_01 = bc.sql('SELECT * FROM main.data_01_parquet').get()
 gdf_01 = query_01.columns
 bc.create_table('data_01', gdf_01)
@@ -17,7 +17,7 @@ bc.create_table('data_01', gdf_01)
 # Gdf
 column_names = ['compactness', 'symmetry', 'fractal_dimension']
 column_types = ['float32', 'float32', 'float32', 'float32']
-gdf_02= cudf.read_csv('/blazingdb/data/cancer_data_02.csv',delimiter=',', dtype=column_types, names=column_names)
+gdf_02= cudf.read_csv('/blazingsql/data/cancer_data_02.csv',delimiter=',', dtype=column_types, names=column_names)
 bc.create_table('data_02', gdf_02)
 
 # Query
